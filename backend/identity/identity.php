61ba8cc9eee6722afd2b4d0f4804573ab6d6b52c
erwan.goalou@gmail.com <>
2009-11-17 15:30:14 +0100

    fix warning when your profil have $restriction == NO

--- backend/identity/identity.php
+++ backend/identity/identity.php
@@ -27,7 +27,7 @@ if (!isset($_SESSION['OCS']["lvluser"])){
 	
 }
 
-if (!isset($tab_tag)){
+if (!isset($tab_tag) and $restriction != 'NO'){
 	$LIST_ERROR="";
 	foreach ($tab_error as $script=>$error){
 			$LIST_ERROR.="<font color=red size=5>".$error."</font><br>";		
