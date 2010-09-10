2b003cc3442057f75534d2e96f0e0f2267cfa941
airoine <erwan.goalou@gmail.com>
2010-09-10 15:22:54 +0200

    use tab for this part

--- plugins/main_sections/ms_regconfig/ms_regconfig.php
+++ plugins/main_sections/ms_regconfig/ms_regconfig.php
@@ -1,91 +1,86 @@
 <?php
-$list_registry_key=array('HKEY_CLASSES_ROOT',
-						 'HKEY_CURRENT_USER',
-						 'HKEY_LOCAL_MACHINE',
-						 'HKEY_USERS',
-						 'HKEY_CURRENT_CONFIG',
-						 'HKEY_DYN_DATA (Windows 9X only)');
-//require_once('require/function_registry.php');
-//cas d'une suppression d'une cl� de registre
-if (isset($protectedPost['SUP_PROF']) and $protectedPost['SUP_PROF'] != ''){	
-	$sql_reg="delete from regconfig where id='%s'";
-	$arg_reg=array($protectedPost['SUP_PROF']);
-	mysql2_query_secure($sql_reg,$_SESSION['OCS']["writeServer"],$arg_reg);
-	$tab_options['CACHE']='RESET';
-}
-
-//cas de ajout/modification d'une cl�
-if (isset($protectedPost['Valid_modif_x'])){
-	if (trim($protectedPost["NAME"])!= "" and 
-		trim($protectedPost["REGTREE"])!= "" and
-		trim($protectedPost["REGKEY"])!= "" and
-		trim($protectedPost["REGVALUE"])!= "")
-	{
-		unset($req);
-		if (isset($protectedPost['id'])){
-			$req = "UPDATE regconfig SET ".	
-				"NAME='%s',".
-				"REGTREE='%s',".
-				"REGKEY='%s',".
-				"REGVALUE='%s' ".
-				"where ID='%s'";
-			$arg_req=array($protectedPost["NAME"],$protectedPost["REGTREE"],
-						   $protectedPost["REGKEY"],$protectedPost["REGVALUE"],
-						   $protectedPost['id']);
-		}else{
-			$sql_verif="select ID from regconfig 
-						where REGTREE='%s' 
-							and REGKEY='%s'
-							and REGVALUE='%s'";
-			$arg_verif=array($protectedPost["REGTREE"],$protectedPost["REGKEY"],$protectedPost['MODIF'],$protectedPost["REGVALUE"]);
-			$res=mysql2_query_secure($sql_verif, $_SESSION['OCS']["readServer"],$arg_verif);
-			$row=mysql_fetch_object($res);
-			if (!is_numeric($row->ID)){				
-			$req = "INSERT INTO regconfig (NAME,REGTREE,REGKEY,REGVALUE)
-					VALUES('%s','%s','%s','%s')";
-			$arg_req=array($protectedPost["NAME"],$protectedPost["REGTREE"],
-						   $protectedPost["REGKEY"],$protectedPost["REGVALUE"]);
-			}else
-			$error=$l->g(987);
-			
-		}
-		
-		if (isset($req)){
-			mysql2_query_secure($req,$_SESSION['OCS']["writeServer"],$arg_req);
-			$tab_options['CACHE']='RESET';
-		}
-	}else{
-		$error=$l->g(988);
-		
-		
-	}
-	
-}
-if (isset($error)){
-	if (isset($protectedPost['id']))
-		$protectedPost['MODIF']=$protectedPost['id'];
-		$protectedPost['ajout']='ADD';
-	echo "<font color=red><b>".$error."</b></font>";
-}
-
-//cas d'une modification d'une cl� de registre
+require_once('require/function_regconfig.php');
+$tab_hidden=array();
+$tab['VIEW']=$l->g(1059);
+$tab['ADD']=$l->g(1060);
+$form_name="registry";
+$table_name="registry";
+echo "<form name='".$form_name."' id='".$form_name."' method='POST' action=''>";
 
 if (isset($protectedPost['MODIF']) and $protectedPost['MODIF'] != ''){	
-	if (!isset($protectedPost['NAME'])){
-		$sql="select * from regconfig where id = '".$protectedPost['MODIF']."'";
+		$protectedPost['tab'] = 'ADD';
+		$sql="select NAME,REGTREE,REGKEY,REGVALUE,ID from regconfig where id = '%s'";
+		$arg=$protectedPost['MODIF'];
 		//$sql="select NAME,ID,MASK from subnet where netid='".$netid."'";
-		$res=mysql_query($sql, $_SESSION['OCS']["readServer"]) or die(mysql_error($_SESSION['OCS']["readServer"]));
+		$res=mysql2_query_secure($sql, $_SESSION['OCS']["readServer"],$arg);
 		$row=mysql_fetch_object($res);
 		$protectedPost['NAME']=$row->NAME;
 		$protectedPost['REGTREE']=$row->REGTREE;
 		$protectedPost['REGKEY']=$row->REGKEY;
 		$protectedPost['REGVALUE']=$row->REGVALUE;
-	}
-	$title=$l->g(108);
-	$tab_hidden['id']=$row->ID;
+		$tab_hidden['id']=$row->ID;	
 }
 
-if (isset($tab_hidden['id']) or isset($protectedPost['ajout'])){	
+onglet($tab,$form_name,"tab",4);
+echo '<div class="mlt_bordure" >';
+if ($protectedPost['tab'] == 'VIEW'){
+	
+	//delete register key
+	if (isset($protectedPost['SUP_PROF']) and $protectedPost['SUP_PROF'] != ''
+		or isset($protectedPost['del_check']) and $protectedPost['del_check'] != ''){			
+			$del=$protectedPost['SUP_PROF'];
+			if ($del == '')
+				$del=$protectedPost['del_check'];
+			delkey($del);
+			$tab_options['CACHE']='RESET';
+	}	
+	
+	$list_fields = array ( 'ID'   => "id", 
+						   $l->g(49) => "name", 
+						   'REGTREE' => "REGTREE",
+						   'REGKEY' => "REGKEY",
+						   'REGVALUE' => "REGVALUE",
+						   'SUP' => "id",
+						   'MODIF' => "id",
+						   'CHECK'=>'id');	
+	$list_col_cant_del=array('SUP'=>'SUP','MODIF' => 'MODIF',$l->g(49)=> $l->g(49),'CHECK'=>'CHECK');
+	$default_fields= array($l->g(49) => "name", 
+						   'REGTREE' => "REGTREE",
+						   'REGKEY' => "REGKEY",
+						   'REGVALUE' => "REGVALUE",
+						   'SUP' => "id",
+						   'MODIF' => "id",
+						   'CHECK'=>"id");
+	$sql=prepare_sql_tab($list_fields,array('SUP','MODIF','CHECK'));
+	$tab_options['ARG_SQL']=$sql['ARG'];
+	$sql['SQL'] .= " from regconfig ";
+	$tab_options['FILTRE']['name']=$l->g(49);
+	$tab_options['FILTRE']['REGKEY']='REGKEY';
+	$tab_options['FILTRE']['REGVALUE']='REGVALUE';
+	$tab_options['REPLACE_VALUE']['REGTREE']=$list_registry_key;
+	$tab_options['LBL_POPUP']['SUP']='name';
+	tab_req($table_name,$list_fields,$default_fields,$list_col_cant_del,$sql['SQL'],$form_name,95,$tab_options);		
+	$img['image/sup_search.png']=$l->g(162);
+	del_selection($form_name);
+	
+}elseif ($protectedPost['tab'] == 'ADD'){
+
+	if (isset($protectedPost['Valid_modif_x'])){
+		$form_values=array("NAME"=>$protectedPost["NAME"],
+						   "REGTREE"=>$protectedPost["REGTREE"],
+						   "REGKEY"=>$protectedPost["REGKEY"],
+						   "REGVALUE"=>$protectedPost["REGVALUE"]);
+		if (isset($protectedPost['id']) and $protectedPost['id'] != '')
+			$udpate=$protectedPost['id'];
+		else
+			$udpate=FALSE;
+		$result=add_update_key($form_values,$udpate);
+		if ($result){
+			unset($_SESSION['OCS']['DATA_CACHE'][$table_name]);
+			unset($_SESSION['OCS']['NUM_ROW'][$table_name]);		
+		}		
+	}	
+	
 	$tab_typ_champ[0]['DEFAULT_VALUE']=$protectedPost['NAME'];
 	$tab_typ_champ[0]['INPUT_NAME']="NAME";
 	$tab_typ_champ[0]['CONFIG']['SIZE']=60;
@@ -107,53 +102,10 @@ if (isset($tab_hidden['id']) or isset($protectedPost['ajout'])){
 	$tab_typ_champ[3]['CONFIG']['MAXLENGTH']=255;
 	$tab_typ_champ[3]['INPUT_TYPE']=0;
 	$tab_name[3]=$l->g(255).": ";
-	$tab_hidden['FILTRE']=$protectedPost['FILTRE'];
-	$tab_hidden['pcparpage']=$protectedPost['pcparpage'];
-	$tab_hidden['page']=$protectedPost['page'];
-	$tab_hidden['old_pcparpage']=$protectedPost['old_pcparpage'];
-	$tab_hidden['tri2']=$protectedPost['tri2'];
-	tab_modif_values($tab_name,$tab_typ_champ,$tab_hidden,$title,$comment="");
+	tab_modif_values($tab_name,$tab_typ_champ,$tab_hidden);
+		
 }
-
-
-
-
-
-
-	$form_name="registry";
-	$table_name="registry";
-	echo "<form name='".$form_name."' id='".$form_name."' method='POST' action=''>";
-	$list_fields = array ( 'ID'   => "id", 
-						   $l->g(49) => "name", 
-						   'REGTREE' => "REGTREE",
-						   'REGKEY' => "REGKEY",
-						   'REGVALUE' => "REGVALUE",
-						   'SUP' => "id",
-						   'MODIF' => "id");
-	
-	$list_col_cant_del=array('SUP'=>'SUP','MODIF' => 'MODIF',$l->g(49)=> $l->g(49));
-	$default_fields= array($l->g(49) => "name", 
-						   'REGTREE' => "REGTREE",
-						   'REGKEY' => "REGKEY",
-						   'REGVALUE' => "REGVALUE",
-						   'SUP' => "id",
-						   'MODIF' => "id");
-	$queryDetails  = "SELECT  ";
-	foreach ($list_fields as $lbl=>$value){
-		if ($lbl != 'SUP' and $lbl != 'MODIF'){
-			$queryDetails .= $value;
-				$queryDetails .=",";		
-		}
-	}
-	$queryDetails  = substr($queryDetails,0,-1)." from regconfig ";
-	$tab_options['FILTRE']['name']=$l->g(49);
-	$tab_options['FILTRE']['REGKEY']='REGKEY';
-	$tab_options['FILTRE']['REGVALUE']='REGVALUE';
-	$tab_options['REPLACE_VALUE']['REGTREE']=$list_registry_key;
-	$tab_options['LBL_POPUP']['SUP']='name';
-	tab_req($table_name,$list_fields,$default_fields,$list_col_cant_del,$queryDetails,$form_name,95,$tab_options);
-	echo "<br><input  class='bouton' name='ajout' type='submit' value='".$l->g(116)."' >";
-
-	echo "</form>";
+echo "</div>";
+echo "</form>";
 
 ?>
