a29344c44dc463ab33fcf2132b1d6f1230a9effc
airoine <erwan.goalou@gmail.com>
2010-09-13 14:48:31 +0200

    change your account informations

--- plugins/main_sections/ms_users/ms_config_account.php
+++ plugins/main_sections/ms_users/ms_config_account.php
@@ -7,7 +7,9 @@ if (isset($protectedPost['Valid_modif_x'])){
 	$protectedPost['MODIF']=$_SESSION['OCS']["loggeduser"];
 	$msg=add_user($protectedPost);	
 	if ($msg != $l->g(374))
-		echo "<script>alert('".$msg."')</script>";
+		msg_error($msg);
+	else
+		msg_success($l->g(1186));
 }
 $form_name="pass";
 echo "<br><form name=".$form_name." action=# method=post>";
