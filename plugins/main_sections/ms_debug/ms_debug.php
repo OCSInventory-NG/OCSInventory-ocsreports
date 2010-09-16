21e6be4db8de7efcf7e39e5480d04568c9db331a
airoine <erwan.goalou@gmail.com>
2010-09-16 17:47:36 +0200

    modify FUSER function

--- plugins/main_sections/ms_debug/ms_debug.php
+++ plugins/main_sections/ms_debug/ms_debug.php
@@ -8,18 +8,22 @@ $list_mode[1]=$l->g(1010);
 $list_mode[2]=$l->g(1011);
 $list_mode[3]=$l->g(1012);
 $list_mode[4]=$l->g(1013);
-$list_mode[5]='FUSER';
+if (isset($_SESSION['OCS']['TRUE_USER'])){
+	$list_mode[5]='NOFUSER';	
+}else
+	$list_mode[5]='FUSER';	
+
 if ($_SESSION['OCS']["usecache"] == 1)
 	$list_mode[6]='NOCACHE';
 else
 	$list_mode[6]='CACHE';
-echo "<br><br><br>";	
+
 $tab_typ_champ[0]['DEFAULT_VALUE']=$list_mode;
 	$tab_typ_champ[0]['INPUT_NAME']="MODE";
 	$tab_typ_champ[0]['INPUT_TYPE']=2;
 	$tab_name[0]=$l->g(1014).":";
 	$tab_typ_champ[0]['RELOAD']="CHANGE";
-if ($protectedPost['MODE'] == 5){
+if ($protectedPost['MODE'] == 5 and $list_mode[$protectedPost['MODE']] == 'FUSER'){
 	$tab_typ_champ[1]['DEFAULT_VALUE']=$protectedPost['FUSER'];
 	$tab_typ_champ[1]['INPUT_NAME']="FUSER";
 	$tab_typ_champ[1]['INPUT_TYPE']=0;
@@ -50,24 +54,24 @@ if (isset($protectedPost['Valid_modif_x']) and $protectedPost["MODE"] != ""){
 	elseif ($protectedPost["MODE"] == 4){
 		$_SESSION['OCS']['MODE_LANGUAGE']="ON";	
 		$_SESSION['OCS']['DEBUG']="ON";
-	}elseif ($protectedPost["MODE"] == 5 and $protectedPost["FUSER"] != ""){
+	}elseif ($protectedPost["MODE"] == 5){
 		if (!isset($_SESSION['OCS']['TRUE_USER'])){
 			$true_user=$_SESSION['OCS']['loggeduser'];
 			$list_page_profil=$_SESSION['OCS']['PAGE_PROFIL'];
 			$restriction=$_SESSION['OCS']['RESTRICTION']['GUI'];
+			$loggeduser=$protectedPost["FUSER"];
+			unset($_SESSION['OCS']);	
+			$_SESSION['OCS']['TRUE_USER']=$true_user;
+			$_SESSION['OCS']['TRUE_PAGES']=$list_page_profil;
+			$_SESSION['OCS']['TRUE_RESTRICTION']=$restriction;
+			$_SESSION['OCS']['loggeduser']=$loggeduser;
+		}else{
+			$loggeduser=$_SESSION['OCS']['TRUE_USER'];
+			$restriction=$_SESSION['OCS']['TRUE_RESTRICTION'];
+			unset($_SESSION['OCS']);		
+			$_SESSION['OCS']['loggeduser']=$loggeduser;
+			$_SESSION['OCS']['RESTRICTION']['GUI']=$restriction;
 		}
-		$loggeduser=$protectedPost["FUSER"];
-		unset($_SESSION['OCS']);	
-		$_SESSION['OCS']['TRUE_USER']=$true_user;
-		$_SESSION['OCS']['TRUE_PAGES']=$list_page_profil;
-		$_SESSION['OCS']['TRUE_RESTRICTION']=$restriction;
-		$_SESSION['OCS']['loggeduser']=$loggeduser;
-	}elseif ($protectedPost["MODE"] == 5 and $protectedPost["FUSER"] == ""){
-		$loggeduser=$_SESSION['OCS']['TRUE_USER'];
-		$restriction=$_SESSION['OCS']['TRUE_RESTRICTION'];
-		unset($_SESSION['OCS']);		
-		$_SESSION['OCS']['loggeduser']=$loggeduser;
-		$_SESSION['OCS']['RESTRICTION']['GUI']=$restriction;
 	}elseif ($protectedPost["MODE"] == 6){
 		 if (isset($_SESSION['OCS']["usecache"]) 
 		 	and $_SESSION['OCS']["usecache"] == 1)		 	
