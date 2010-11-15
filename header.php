b54ba9b0c2d80eb6e033b88780881ec59323d198
airoine <erwan.goalou@gmail.com>
2010-11-15 09:20:29 +0100

    fix warning. Tx to Damien!

--- header.php
+++ header.php
@@ -233,9 +233,11 @@ if (!isset($_SESSION['OCS']["use_redistribution"])){
 if (!isset($_SESSION['OCS']['TAG_LBL'])){
 	require_once('require/function_admininfo.php');
 	$all_tag_lbl=witch_field_more('COMPUTERS');
-	foreach ($all_tag_lbl['LIST_NAME'] as $key=>$value){
-		$_SESSION['OCS']['TAG_LBL'][$value]=$all_tag_lbl['LIST_FIELDS'][$key];
-		$_SESSION['OCS']['TAG_ID'][$key]=$value;
+	if (is_array($all_tag_lbl)){
+		foreach ($all_tag_lbl['LIST_NAME'] as $key=>$value){
+			$_SESSION['OCS']['TAG_LBL'][$value]=$all_tag_lbl['LIST_FIELDS'][$key];
+			$_SESSION['OCS']['TAG_ID'][$key]=$value;
+		}
 	}
 }
 /*******************************************GESTION OF PLUGINS (MAIN SECTIONS)****************************/
