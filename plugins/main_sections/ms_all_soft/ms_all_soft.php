d6f0418f7c2329dbb6d844b4646adfde7279f387
airoine <erwan.goalou@gmail.com>
2010-12-21 15:06:52 +0100

    new version of all soft

--- plugins/main_sections/ms_all_soft/ms_all_soft.php
+++ plugins/main_sections/ms_all_soft/ms_all_soft.php
@@ -8,6 +8,13 @@
 // code is always made freely available.
 // Please refer to the General Public Licence http://www.gnu.org/ or Licence.txt
 //====================================================================================
+function array_merge_values($arr,$arr2){
+	foreach ($arr2 as $key=>$values){
+		array_push($arr,$values);		
+	}
+	return $arr;
+}
+
 
 
 if ($protectedPost['RESET']){ 
@@ -19,44 +26,63 @@ if ($protectedPost['RESET']){
 if ($protectedPost['SUBMIT_FORM'])
 $tab_options['CACHE']='RESET';
 
-$sql_fin="";
-
-$sql_list_alpha ="select substr(trim(name),1,1) alpha, name ";
+$sql_fin['SQL']="";
+$sql_fin['ARG']=array();
+$sql_list_alpha['ARG']=array();
+$sql_list_alpha['SQL'] ="select substr(trim(name),1,1) alpha, name ";
 if (isset($protectedPost['NBRE']) and $protectedPost['NBRE'] != "" and isset($protectedPost['COMPAR']) and $protectedPost['COMPAR'] != ""){
-	$sql_list_alpha .=",count(*) nb ";	
-	$sql_fin=" having nb ".$protectedPost['COMPAR']." ".$protectedPost['NBRE']." ";
+	$sql_list_alpha['SQL'] .=",count(*) nb ";	
+	$sql_fin['SQL']=" having nb %s %s ";
+	$sql_fin['ARG']=array($protectedPost['COMPAR'],$protectedPost['NBRE']);
 }
-$sql_list_alpha .=" from ";
+$sql_list_alpha['SQL'] .=" from ";
 $and_where="";
 if ($_SESSION['OCS']["usecache"] == 1  and $protectedPost['NBRE'] == ""){
-	$sql_list_alpha .=" softwares_name_cache left join dico_soft on dico_soft.extracted=softwares_name_cache.name ";
+	$sql_list_alpha['SQL'] .=" softwares_name_cache left join dico_soft on dico_soft.extracted=softwares_name_cache.name ";
 	$and_where=" where ";
 }else{
-	$sql_list_alpha .=" softwares left join dico_soft on dico_soft.extracted=softwares.name ";
+	$sql_list_alpha['SQL'] .=" softwares left join dico_soft on dico_soft.extracted=softwares.name ";
 	if ($_SESSION['OCS']["mesmachines"] != ""){
-		$sql_list_alpha .=",accountinfo a where ".$_SESSION['OCS']["mesmachines"]." and a.hardware_id=softwares.HARDWARE_ID ";
+		$sql_list_alpha['SQL'] .=",accountinfo a where ".$_SESSION['OCS']["mesmachines"]." and a.hardware_id=softwares.HARDWARE_ID ";
 		$and_where=" and ";
 	}else
 	$and_where=" where ";
 }
-$sql_list_alpha .=$and_where." substr(trim(name),1,1) is not null ";
-if (isset($protectedPost['NAME_RESTRICT']) and $protectedPost['NAME_RESTRICT'] != "")
-	$sql_list_alpha .=" and name like '%".$protectedPost['NAME_RESTRICT']."%' ";
-if (isset($protectedPost['CLASS']) and $protectedPost['CLASS'] != ""){
-		$sql_list_alpha.=" and (dico_soft.formatted in ('".implode("','",$list_soft_by_statut[$protectedPost['CLASS']])."') ".$sql_default." ) ";		
-	}
-$sql_list_alpha .=" group by name ".$sql_fin;
+$sql_list_alpha['SQL'] .=$and_where." substr(trim(name),1,1) is not null ";
+if (isset($protectedPost['NAME_RESTRICT']) and $protectedPost['NAME_RESTRICT'] != ""){
+	$sql_list_alpha['SQL'] .=" and name like '%s' ";
+	$sql_list_alpha['ARG']=array('%'.$protectedPost['NAME_RESTRICT'].'%');	
+}
+/*if (isset($protectedPost['CLASS']) and $protectedPost['CLASS'] != ""){
+		$sql_list_alpha['SQL'].=" and (dico_soft.formatted in ";	
+		$sql_list_alpha=mysql2_prepare($sql_list_alpha['SQL'],$sql_list_alpha['ARG'],$list_soft_by_statut[$protectedPost['CLASS']]);	
+		$sql_list_alpha['SQL'] .=" ) ";
+	}*/
+$sql_list_alpha['SQL'] .=" group by name ".$sql_fin['SQL'];
+//if ($sql_list_alpha['ARG'] != array() and $sql_fin['ARG'] != array())
+	$sql_list_alpha['ARG']=array_merge_values($sql_list_alpha['ARG'],$sql_fin['ARG']);
+//elseif ($sql_fin['ARG'] != array() and $sql_list_alpha['ARG'] == array())
+//	$sql_list_alpha['ARG']=$sql_fin['ARG'];
 
+	
+/*p($sql_fin['ARG']);
+p($sql_list_alpha['ARG']);*/
 //execute the query only if necessary 
 if($_SESSION['OCS']['REQ_ONGLET_SOFT'] != $sql_list_alpha or !isset($protectedPost['onglet'])){
-	$result_list_alpha = mysql_query( $sql_list_alpha, $_SESSION['OCS']["readServer"]);
+	$result_list_alpha = mysql2_query_secure( $sql_list_alpha['SQL'], $_SESSION['OCS']["readServer"],$sql_list_alpha['ARG']);
  	while($item_list_alpha = mysql_fetch_object($result_list_alpha)){
- 		if (strtoupper($item_list_alpha -> alpha) != ""){
+ 		
+ 		if (strtoupper($item_list_alpha -> alpha) == '"')
+					$car="'";
+				else
+					$car=strtoupper($item_list_alpha -> alpha);
+					
+ 		if ($car != ""){
 				if (!isset($protectedPost['onglet']))
-					$protectedPost['onglet']=strtoupper($item_list_alpha -> alpha);
-				$list_alpha[strtoupper($item_list_alpha -> alpha)]=strtoupper($item_list_alpha -> alpha);
+					$protectedPost['onglet']=$car;
+				$list_alpha[$car]=$car;
 				if (!isset($first)){
-					$first=$list_alpha[strtoupper($item_list_alpha -> alpha)];				
+					$first=$car;				
 				}
  		}
 	}
@@ -80,63 +106,74 @@ if ((isset($protectedPost['NAME_RESTRICT']) and $protectedPost['NAME_RESTRICT'] 
 
 //utilisation du cache
 if ($_SESSION['OCS']["usecache"] == 1){
-	$search_soft="select name from softwares_name_cache left join dico_soft on dico_soft.extracted=softwares_name_cache.name ";
-	$forcedRequest=$search_soft;
-	$search_soft.=" where name like '".$protectedPost['onglet']."%'";
+	$search_soft['SQL']="select name from softwares_name_cache ";
+	//$forcedRequest['SQL']=$search_soft['SQL'];
+	$search_soft['SQL'].=" where name like '%s'";
+	$search_soft['ARG']=array($protectedPost['onglet']."%");
 	$and_where=" where ";
 	if (isset($protectedPost['NAME_RESTRICT']) and $protectedPost['NAME_RESTRICT'] != ""){
-		$forcedRequest.= $and_where." name like '%".$protectedPost['NAME_RESTRICT']."%' ";
-		$search_soft.=" and name like '%".$protectedPost['NAME_RESTRICT']."%' ";		
+		//$forcedRequest['SQL'].= $and_where." name like '%s' ";
+		//$forcedRequest['ARG']=array("%".$protectedPost['NAME_RESTRICT']."%");
+		$search_soft['SQL'].=" and name like '%s' ";	
+		array_push($search_soft['ARG'],"%".$protectedPost['NAME_RESTRICT']."%");	
 		$and_where=" and ";
 	}
-	if (isset($protectedPost['CLASS']) and $protectedPost['CLASS'] != ""){
+
+	/*if (isset($protectedPost['CLASS']) and $protectedPost['CLASS'] != ""){
 	//	$fin_sql=" and dico_soft.extracted is not null ";
-		$forcedRequest.= $and_where." (dico_soft.formatted in ('".implode("','",$list_soft_by_statut[$protectedPost['CLASS']])."') ".$sql_default." ) and ";
-		$search_soft.=" and (dico_soft.formatted in ('".implode("','",$list_soft_by_statut[$protectedPost['CLASS']])."') ".$sql_default." ) ";		
-	}
+		$forcedRequest.= $and_where." (dico_soft.formatted in ('".implode("','",$list_soft_by_statut[$protectedPost['CLASS']])."') ) and ";
+		$search_soft.=" and (dico_soft.formatted in ('".implode("','",$list_soft_by_statut[$protectedPost['CLASS']])."') ) ";		
+	}*/
 	//echo $search_soft;
-	$result_search_soft = mysql_query( $search_soft, $_SESSION['OCS']["readServer"]);
+	$result_search_soft = mysql2_query_secure( $search_soft['SQL'], $_SESSION['OCS']["readServer"],$search_soft['ARG']);
 	$list_soft="";
 	//$count_soft=0;
 	while($item_search_soft = mysql_fetch_object($result_search_soft)){
-		$list_soft[]=mysql_escape_string($item_search_soft->name);	
+		$list_soft[]=$item_search_soft->name;	
 		//$count_soft++;	
 	}
 }
 if ($list_soft != ""){
 	$and_where="";
-	$sql="select  name , count(name) nb from softwares ";
+	$sql['SQL']="select  name , count(name) nb from softwares ";
 
 	if (isset($_SESSION['OCS']["mesmachines"]) and $_SESSION['OCS']["mesmachines"] != ''){
-		$sql.=",accountinfo a where ".$_SESSION['OCS']["mesmachines"]." and a.hardware_id=softwares.HARDWARE_ID";
+		$sql['SQL'].=",accountinfo a where ".$_SESSION['OCS']["mesmachines"]." and a.hardware_id=softwares.HARDWARE_ID";
 		$and_where=" and ";
 	}else
 	$and_where=" where ";	
-	$_SESSION['OCS']["forcedRequest"]=$sql.$and_where." name in (".$forcedRequest.")";
-	$sql.=$and_where." name in ('".implode("','",$list_soft)."')";
+	//$_SESSION['OCS']["forcedRequest"]=$sql['SQL'].$and_where." name in (".$forcedRequest.")";
+	$sql['SQL'].=$and_where." name in ";
+	$sql['ARG']=array();
+	$sql=mysql2_prepare($sql['SQL'],$sql['ARG'],$list_soft);
+	//$sql['ARG']=('".implode("','",$list_soft)."')";
 	//$sql.=$fin_sql;
 }else{
 	$and_where="";
-	$sql="select  name, count(name) nb from softwares ";
+	$sql['SQL']="select  name, count(name) nb from softwares ";
+	$sql['ARG']=array();
 	if (isset($_SESSION['OCS']["mesmachines"]) and $_SESSION['OCS']["mesmachines"] != ''){
-		$sql.=",accountinfo a where ".$_SESSION['OCS']["mesmachines"]." and a.hardware_id=softwares.HARDWARE_ID";
+		$sql['SQL'].=",accountinfo a where ".$_SESSION['OCS']["mesmachines"]." and a.hardware_id=softwares.HARDWARE_ID";
 		$and_where=" and ";
 	}else
 	$and_where=" where ";
-	$_SESSION['OCS']["forcedRequest"]=$sql;
-	$sql.=$and_where." name like '".$protectedPost['onglet']."%'";
+	//$_SESSION['OCS']["forcedRequest"]=$sql;
+	$sql['SQL'].=$and_where." name like '%s'";
+	array_push($sql['ARG'],$protectedPost['onglet']."%");
 	if (isset($protectedPost['NAME_RESTRICT']) and $protectedPost['NAME_RESTRICT'] != ""){
-		$sql.=" and name like '%".$protectedPost['NAME_RESTRICT']."%' ";	
-		$_SESSION['OCS']["forcedRequest"].=$and_where."name like '%".$protectedPost['NAME_RESTRICT']."%'" ;
+		$sql['SQL'].=" and name like '%s' ";	
+		array_push($sql['ARG'],"%".$protectedPost['NAME_RESTRICT']."%");
+		//$_SESSION['OCS']["forcedRequest"].=$and_where."name like '%".$protectedPost['NAME_RESTRICT']."%'" ;
 	}
 }
 
 if (isset($sql)){
-	$sql.=" group by name";
-	$_SESSION['OCS']["forcedRequest"].=" group by name";
-	if ($sql_fin != ''){
-	$sql.=$sql_fin;
-	$_SESSION['OCS']["forcedRequest"].=$sql_fin;
+	$sql['SQL'].=" group by name";
+//	$_SESSION['OCS']["forcedRequest"].=" group by name";
+	if ($sql_fin['SQL'] != ''){
+		$sql['SQL'].=$sql_fin['SQL'];
+		$sql['ARG']= array_merge_values($sql['ARG'],$sql_fin['ARG']);
+//	$_SESSION['OCS']["forcedRequest"].=$sql_fin;
 	}
 	$list_fields= array('name'=>'name',
 						'nbre'=>'nb'
@@ -147,8 +184,9 @@ if (isset($sql)){
 	$tab_options['LIEN_CHAMP']['nbre']='name';
 	$tab_options['LBL']['name']=$l->g(847);
 	$tab_options['LBL']['nbre']=$l->g(1120);
+	$tab_options['ARG_SQL']=$sql['ARG'];
 	//$tab_options['nbre'][]
-	$result_exist=tab_req($table_name,$list_fields,$default_fields,$list_col_cant_del,$sql,$form_name,80,$tab_options); 
+	$result_exist=tab_req($table_name,$list_fields,$default_fields,$list_col_cant_del,$sql['SQL'],$form_name,80,$tab_options); 
 }
 
 echo "<br><div class='mvt_bordure'>";
@@ -158,9 +196,9 @@ echo $l->g(382).": ".show_modif($protectedPost['NAME_RESTRICT'],'NAME_RESTRICT',
 echo "&nbsp;".$l->g(381).": ".show_modif(array('<'=>'<','>'=>'>','='=>'='),'COMPAR',2);
 echo show_modif($protectedPost['NBRE'],'NBRE',0,'',array('MAXLENGTH'=>100,'SIZE'=>10,'JAVASCRIPT'=>$numeric));
 //echo "<input type='input' name='NBRE' value='".$protectedPost['NBRE']."' ".$numeric.">";
-	echo "<br><a href='index.php?".PAG_INDEX."=".$pages_refs['ms_soft_csv']."&no_header=1'>".$l->g(183)." ".$l->g(765)."</a>";
+	echo "<br><a href='index.php?".PAG_INDEX."=".$pages_refs['ms_soft_csv']."&no_header=1&soft=".$protectedPost['NAME_RESTRICT']."&nb=".$protectedPost['NBRE']."&comp=".htmlentities($protectedPost['COMPAR'])."'>".$l->g(183)." ".$l->g(765)."</a>";
 if ($protectedPost['COMPAR'] == '<' and $protectedPost['NBRE']<=15 and $protectedPost['NBRE'] != "")
-echo "<br><a href='index.php?".PAG_INDEX."=".$pages_refs['ms_exportallsoft']."&no_header=1'>".$l->g(912)."</a>";		
+echo "<br><a href='index.php?".PAG_INDEX."=".$pages_refs['ms_soft_csv']."&no_header=1&soft=".$protectedPost['NAME_RESTRICT']."&nb=".$protectedPost['NBRE']."&comp=".htmlentities($protectedPost['COMPAR'])."&all_computers=yes'>".$l->g(912)."</a>";		
 	echo "<br><input type='submit' value='".$l->g(393)."' name='SUBMIT_FORM'><input type='submit' value='".$l->g(396)."' name='RESET'>";
 
 echo '</div>';
--- plugins/main_sections/ms_export/ms_soft_csv.php
+++ plugins/main_sections/ms_export/ms_soft_csv.php
@@ -9,30 +9,92 @@
 // Please refer to the General Public Licence http://www.gnu.org/ or Licence.txt
 //====================================================================================
 //Modified on $Date: 2010 $$Author: Erwan Goalou
+$compartruevalue=array('<','>','=','');
+if (!in_array($protectedGet['comp'],$compartruevalue))
+	die();
+
 $values=look_config_default_values(array('EXPORT_SEP'));
 if (isset($values['tvalue']['EXPORT_SEP']) and $values['tvalue']['EXPORT_SEP'] != '')
 	$separator=$values['tvalue']['EXPORT_SEP'];
 else
 	$separator=';';
+//use cache?
+if ($_SESSION['OCS']["usecache"] == 1){
+	$sql['SQL']="select name from softwares_name_cache ";
+	if (isset($protectedGet['soft']) and $protectedGet['soft'] != ''){
+		$sql['SQL'].= " where name like '%s'";
+		$sql['ARG']=array('%'.$protectedGet['soft'].'%');		
+	}
+	$result_search_soft = mysql2_query_secure( $sql['SQL'], $_SESSION['OCS']["readServer"],$sql['ARG']);
+	$list_soft="";
+	while($item_search_soft = mysql_fetch_object($result_search_soft)){
+		$list_soft[]=$item_search_soft->name;	
+	}
+	$sql['SQL']="select count(*) nb, name from softwares s,accountinfo a where a.hardware_id=s.hardware_id and name in ";
+	$sql['ARG']=array();
+	$sql=mysql2_prepare($sql['SQL'],$sql['ARG'],$list_soft);
 
+	if ($_SESSION['OCS']['TAGS'] != ""){
+		$sql['SQL'].= " and a.tag in ";
+		$sql=mysql2_prepare($sql['SQL'],$sql['ARG'],$_SESSION['OCS']['TAGS']);
+	}
+	$sql['SQL'].=" group by name ";
+	if (isset($protectedGet['nb']) and $protectedGet['nb'] != '' and isset($protectedGet['comp']) and $protectedGet['comp'] != ''){
+		$sql['SQL'].= "having nb %s %s";
+		array_push($sql['ARG'],$protectedGet['comp']);
+		array_push($sql['ARG'],$protectedGet['nb']);
+	}	
+}else{
 
-if( isset($_SESSION['OCS']["forcedRequest"] )) {
-	$lareq = $_SESSION['OCS']["forcedRequest"];
+	$sql['SQL']="select count(*) nb, name from softwares s,accountinfo a where a.hardware_id=s.hardware_id ";
+	if (isset($protectedGet['soft']) and $protectedGet['soft'] != ''){
+		$sql['SQL'].= " where name like '%s'";
+		$sql['ARG']=array('%'.$protectedGet['soft'].'%');	
+	}
+	if ($_SESSION['OCS']['TAGS'] != ""){
+		$sql['SQL'].= " and a.tag in ";
+		$sql=mysql2_prepare($sql['SQL'],$sql['ARG'],$_SESSION['OCS']['TAGS']);
+	}
+	$sql['SQL'].=" group by name ";
+
+	if (isset($protectedGet['nb']) and $protectedGet['nb'] != '' and isset($protectedGet['comp']) and $protectedGet['comp'] != ''){
+		$sql['SQL'].= "having nb %s %s";
+		array_push($sql['ARG'],$protectedGet['comp']);
+		array_push($sql['ARG'],$protectedGet['nb']);
+	}	
+	
+	
 }
-else
-	die();//$lareq = $_SESSION['OCS']["storedRequest"]->getFullRequest();
 
-$lareq = str_replace("h.id AS \"h.id\",","",$lareq);
-$lareq = str_replace("deviceid AS \"deviceid\",","",$lareq);
-$lareq = str_replace("h.n.ipmask","n.ipmask",$lareq);
+if (isset($protectedGet['all_computers'])){
+	$fields= array("a.tag"=>$_SESSION['OCS']['TAG_LBL']['TAG'],
+			   "s.name"=>$l->g(20),
+			   "h.name"=>$l->g(23),
+			   "h.userid"=>$l->g(24),
+			   "h.description"=>$l->g(53),
+			   "h.lastdate"=>$l->g(728));
+	$result_search_soft = mysql2_query_secure( $sql['SQL'], $_SESSION['OCS']["readServer"],$sql['ARG']);
+	while($item_search_soft = mysql_fetch_object($result_search_soft)){
+		$soft[]=$item_search_soft->name;
+	}
 
-//echo $lareq;die();
-$result=mysql_query($lareq, $_SESSION['OCS']["readServer"]) or die(mysql_error($_SESSION['OCS']["readServer"]));
-//echo "requete:".$lareq;
-// iexplorer problem
+	$sql=prepare_sql_tab(array_keys($fields));
+	$sql['SQL'].= " from accountinfo a, softwares s,hardware h 
+					where a.hardware_id=h.id and s.hardware_id=h.id ";
+	if (isset($_SESSION['OCS']['TAGS'])){
+			$sql['SQL'].= " and a.tag in ";
+			$sql=mysql2_prepare($sql['SQL'],$sql['ARG'],$_SESSION['OCS']['TAGS']);
+	}
+	//echo $sql['SQL'];
+}else{
+	$fields=array('nb'=>$l->g(55),'name'=>$l->g(20));
+	
+}
+
+
+$toBeWritten=implode(';',$fields)."\r\n";
 if( ini_get("zlib.output-compression"))
 	ini_set("zlib.output-compression","Off");
-	
 header("Pragma: public");
 header("Expires: 0");
 header("Cache-control: must-revalidate, post-check=0, pre-check=0");
@@ -40,25 +102,21 @@ header("Cache-control: private", false);
 header("Content-type: application/force-download");
 header("Content-Disposition: attachment; filename=\"export.csv\"");
 header("Content-Transfer-Encoding: binary");
-
-$toBeWritten = "";
-
-while( $colname = mysql_fetch_field($result) ) {
-	$toBeWritten .=$colname->name.$separator;
-	$cols[] = $colname->name;
-}
-$toBeWritten .="\r\n";
-//writeTab($fp,$cols);
-
-while( $cont = mysql_fetch_array($result,MYSQL_ASSOC) ) {
-	foreach ($cols as $k=>$v){
-		$toBeWritten.=$cont[$v].$separator;
+$result_search_soft = mysql2_query_secure( $sql['SQL'], $_SESSION['OCS']["readServer"],$sql['ARG']);
+while($item_search_soft = mysql_fetch_object($result_search_soft)){
+	foreach ($fields as $key=>$values){
+		$trait=explode('.',$key);
+		if (isset($trait[1]))
+			$fi=$trait[1];
+		else
+			$fi=$trait[0];
+			
+		$toBeWritten.= $item_search_soft->$fi.$separator;
 	}
-	$toBeWritten .="\r\n";
-	//writeTab($fp,$cont,$nameIndex);
+	$toBeWritten.= "\r\n";
+	
 }
-
 header("Content-Length: ".strlen($toBeWritten));
-echo $toBeWritten;
-
+	echo $toBeWritten;
+die();
 ?>
