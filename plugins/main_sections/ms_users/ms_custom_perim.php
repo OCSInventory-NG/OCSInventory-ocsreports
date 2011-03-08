dcdf52b4160b301ea21461eb70fb78e58ddfcb29
airoine <erwan.goalou@gmail.com>
2011-03-08 18:35:03 +0100

    use jokers option for adding tags to a profile

--- plugins/main_sections/ms_users/ms_custom_perim.php
+++ plugins/main_sections/ms_users/ms_custom_perim.php
@@ -16,12 +16,47 @@
  
 
 $form_name='taguser';
+//BEGIN SHOW ACCOUNTINFO
+require_once('require/function_admininfo.php');
+$info_tag=find_info_accountinfo('1','COMPUTERS');
+if (is_array($info_tag)){
+	foreach ($info_tag as $key=>$value){
+		$info_value_tag= accountinfo_tab($value['id']);		
+		if (is_array($info_value_tag)){
+			$tab_options['REPLACE_VALUE'][$value['comment']]=$info_value_tag;
+		}		
+	}
+}
+//END SHOW ACCOUNTINFO
+
 printEnTete($l->g(616)." ".$protectedGet["id"] );
 if( $protectedPost['newtag'] != "" ) {
+	if (isset($protectedPost['use_generic_0'])){
+		if (is_array($info_value_tag)){
+			$arg=str_replace(array("*","?"), "", $protectedPost["newtag"]); 
+			$array_result=find_value_in_field(1,$arg);
+		}else{
+			$arg=str_replace(array("*","?"), array("%","_"), $protectedPost["newtag"]); 
+			$sql="select distinct TAG from accountinfo where TAG like '%s'";
+			$res=mysql2_query_secure($sql,$_SESSION['OCS']["readServer"],$arg);
+			while ($val_account_data = mysql_fetch_array( $res ))
+				$array_result[]=$val_account_data['TAG'];
+		}
+		
+	}else
+		$array_result[]=$protectedPost["newtag"];
+	
+	
 	$tab_options['CACHE']='RESET';
 	$sql="insert into tags (tag,login) values ('%s','%s')";
-	$arg=array($protectedPost["newtag"],$protectedGet["id"]);
-	mysql2_query_secure($sql,$_SESSION['OCS']["writeServer"],$arg);
+	$i=0;
+	while (isset($array_result[$i])){
+		$arg=array($array_result[$i],$protectedGet["id"]);
+		mysql2_query_secure($sql,$_SESSION['OCS']["writeServer"],$arg);
+		$i++;
+	}
+	
+	
 	unset($protectedPost['newtag']);
 }
 //suppression d'une liste de tag
@@ -57,23 +92,11 @@ array_push($sql['ARG'],$protectedGet['id']);
 $tab_options['ARG_SQL']=$sql['ARG'];
 $tab_options['ARG_SQL_COUNT']=$protectedGet['id'];
 $tab_options['FILTRE']=array($_SESSION['OCS']['TAG_LBL']['TAG']=>$_SESSION['OCS']['TAG_LBL']['TAG']);
-//BEGIN SHOW ACCOUNTINFO
-require_once('require/function_admininfo.php');
-$info_tag=find_info_accountinfo('1','COMPUTERS');
-if (is_array($info_tag)){
-	foreach ($info_tag as $key=>$value){
-		$info_value_tag= accountinfo_tab($value['id']);		
-		if (is_array($info_value_tag)){
-			$tab_options['REPLACE_VALUE'][$value['comment']]=$info_value_tag;
-		}		
-	}
-}
-//END SHOW ACCOUNTINFO
 tab_req($table_name,$list_fields,$default_fields,$list_col_cant_del,$sql['SQL'],$form_name,100,$tab_options);
 //traitement par lot
 del_selection($form_name);
 	
-if (is_array($info_value_tag)){
+if (is_array($info_value_tag) and !isset($protectedPost['use_generic_0'])){
 	$type=2;
 }else{
 	$type=0;
@@ -82,7 +105,8 @@ if (is_array($info_value_tag)){
 	
 $select_choise=show_modif($info_value_tag,'newtag',$type);	
 echo $l->g(617)." ".$_SESSION['OCS']['TAG_LBL']['TAG'].": ".$select_choise;
-echo "<input type='submit' name='ADD_TAG' value='" . $l->g(13) . "'>";
+echo "<input type='submit' name='ADD_TAG' value='" . $l->g(13) . "'><br>";
+echo show_modif(array($l->g(358)),'use_generic',5,$form_name);
 echo "</form>";
 ?>
 
