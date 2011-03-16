50357c58645a7fa4974ec8271e732e985c18356c
mortheres <guillaume.protet@mortheres.info>
2011-03-16 23:08:40 +0100

    Fix a bug with the use of pacman tool on ArchLinux. We now launch the real packages manager instead of the famous game ;) ;)

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/Pacman.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/Pacman.pm
@@ -1,12 +1,12 @@
 package Ocsinventory::Agent::Backend::OS::Generic::Packaging::Pacman;
 
-sub check {can_run("pacman")}
+sub check {can_run("/usr/bin/pacman")}
 
 sub run {
   my $params = shift;
   my $common = $params->{common};
 
-  foreach(`pacman -Q`){
+  foreach(`/usr/bin/pacman -Q`){
       /^(\S+)\s+(\S+)/;
       my $name = $1;
       my $version = $2;
