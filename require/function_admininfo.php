2f3af20a2418e2567ee2e5dc4987bc73ce29e2f4
airoine <erwan.goalou@gmail.com>
2011-04-19 14:14:51 +0200

    force SQL format for new accountinfo and new field in wk of teledeploy

--- require/function_admininfo.php
+++ require/function_admininfo.php
@@ -67,6 +67,9 @@ function add_accountinfo($newfield,$newtype,$newlbl,$tab,$type='COMPUTERS'){
 		//msg_error($type);
 		return array('ERROR'=>$type);
 	}
+	//can not contain special characters
+	if(preg_match('/[^0-9A-Za-z]/',$sql_type_accountinfo[$newtype]))
+			return array('ERROR'=> $l->g(1178).' : <i>' . $l->g(1070) . "</i> " . $l->g(1179) . " <br>");
 		
 	$ERROR=dde_exist($newfield,'',$type);
 	$id_order=max_order('accountinfo_config','SHOW_ORDER');	
