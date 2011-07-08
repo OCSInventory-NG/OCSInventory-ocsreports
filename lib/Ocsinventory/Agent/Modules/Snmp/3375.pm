1af2540a32b8063c26483c18d8e00438f3b2a4bc
mortheres <guillaume.protet@mortheres.info>
2011-07-08 16:57:57 +0200

    Now getting MANUFACTURER and SERIALNUMBER in Snmp/3375.pm

--- lib/Ocsinventory/Agent/Modules/Snmp/3375.pm
+++ lib/Ocsinventory/Agent/Modules/Snmp/3375.pm
@@ -20,12 +20,13 @@ sub snmp_run()
   my $common = $snmp->{common};
   my $logger=$snmp->{logger};
 
-  $logger->debug("Running F5 (3375) MIB module");
-  $common->setSnmpCommons( {TYPE => "Load Balanceur"} );
+  $logger->debug("Execution mib F5 mib");
+  $common->setSnmpCommons( {TYPE => "Load Balancer"} );
 
   my $list_mib=["If_Mib"];
 
   foreach my $mib ( @{$list_mib} ) {
+     $logger->debug("Sub mib $mib");
      $snmp->snmp_oid_run($mib);
   }
 
@@ -33,11 +34,15 @@ sub snmp_run()
   my $snmp_sysProductVersion="1.3.6.1.4.1.3375.2.1.4.2.0";
   my $snmp_sysProductBuild="1.3.6.1.4.1.3375.2.1.4.3.0";
   my $snmp_sysProductDate="1.3.6.1.4.1.3375.2.1.4.5.0";
+  my $snmp_sysGeneralHwNumber="1.3.6.1.4.1.3375.2.1.3.3.2.0";
+  my $snmp_sysGeneralChassisSerialNum="1.3.6.1.4.1.3375.2.1.3.3.3.0";
+
   
   my $name=$session->get_request ( -varbindlist => [ $snmp_sysProductName ] );
   if ( defined ( $name ) ) {
      $name=$name->{$snmp_sysProductName};
   }
+
   my $version=$session->get_request ( -varbindlist => [ $snmp_sysProductVersion] );
   if ( defined ( $version ) ) {
      $version=$version->{$snmp_sysProductVersion};
@@ -53,11 +58,26 @@ sub snmp_run()
      $date=$date->{$snmp_sysProductDate};
   }
 
-  $common->addSoftware( { NAME => $name ,
-		 VERSION => $version ,
-		 INSTALLDATE => $date ,
-	         COMMENT => $comment,
-               });
+  $common->addSoftware( {
+    NAME => $name ,
+    VERSION => $version ,
+    INSTALLDATE => $date ,
+    COMMENT => $comment,
+  });
+
+  my $type=$session->get_request ( -varbindlist => [ $snmp_sysGeneralHwNumber ] ) ;
+  if ( defined ( $type ) ) {
+     $type=$type->{$snmp_sysGeneralHwNumber};
+  }
+  my $serialnumber=$session->get_request ( -varbindlist => [ $snmp_sysGeneralChassisSerialNum ] ) ;
+  if ( defined ( $serialnumber ) ) {
+     $serialnumber=$serialnumber->{$snmp_sysGeneralChassisSerialNum};
+  }
+  $common->setSnmpLoadBalancer({
+    SERIALNUMBER => $serialnumber ,
+    TYPE => $type ,
+    MANUFACTURER => "F5" ,
+  });
 
 }
 
