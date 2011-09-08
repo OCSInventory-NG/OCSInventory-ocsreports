87ddaf661e2053daac6f2ab0fac9bb1d8e22a1f6
airoine <erwan.goalou@gmail.com>
2011-09-08 09:54:35 +0200

    revert: 20 result by page

--- var.php
+++ var.php
@@ -29,5 +29,5 @@ define('HEADER_HTML',"require/html_header.php");						// Define html_header file
 define('FOOTER_HTML',"require/footer.php");								// Define footer file		
 define('MAIN_SECTIONS_DIR',PLUGINS_DIR."main_sections/");				//
 define('DEV_OPTION',false);												// Define DEV Options DO NOT USE
-define('PC4PAGE',1000000);												// Define result by page MUST in (5,10,15,20,50,100,200,1000000);
+define('PC4PAGE',20);												// Define result by page MUST in (5,10,15,20,50,100,200,1000000);
 ?>
