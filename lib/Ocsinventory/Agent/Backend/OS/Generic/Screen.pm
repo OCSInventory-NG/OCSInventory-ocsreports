6f0d36662dad3aecfe98948b7fe4e1d42b54a16f
mortheres <guillaume.protet@mortheres.info>
2012-02-27 20:28:25 +0100

    Fix CRLF problems in Modules/Download.pm Fix permissions problems on Modules/Download.pm, Modules/Snmp.pm, Modules/Example.pm and Ho oks.pm files Fix FSF outdated FSF address in Backend/OS/Generic/Screen.pm
    Tanks to Remi Collet for his report

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Screen.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Screen.pm
@@ -13,7 +13,7 @@ package Ocsinventory::Agent::Backend::OS::Generic::Screen;
 
 #     You should have received a copy of the GNU General Public License
 #     along with this program; if not, write to the Free Software
-#     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#     Foundation (website: http://www.fsf.org).
 #
 # Some part come from Mandriva's (great) monitor-edid
 # http://svn.mandriva.com/cgi-bin/viewvc.cgi/soft/monitor-edid/trunk/
