d1d2a0dad41c74101ed37ab45adf0bc15db91470
mortheres <guillaume.protet@mortheres.info>
2012-04-15 01:25:44 +0200

    Code cleaning in tools/macosx/BUILDME.sh Remove useless files and scripts in tools/macosx directory due to OCS MacOSX packager

--- tools/macosx/BUILDME.sh
+++ tools/macosx/BUILDME.sh
@@ -9,10 +9,6 @@ PATCHES_PATH="patches"
 TOOLS_PATH="tools/macosx"
 FINAL_PKG_NAME="unified_unix_agent-macosx"
 
-if [ "$1" == "-release" ]; then
-	RELEASE=1 
-fi
-
 if [ ! -x ../../inc ]; then
 	echo "You're probably building from BZR, you're missing the "inc" directory in ../../"
 	exit 1;
@@ -83,50 +79,24 @@ cp -R darwin-perl-lib/ $OCSNG_PATH/Contents/Resources/lib/
 echo 'copying uninstall script'
 cp scripts/uninstaller.sh $OCSNG_PATH/Contents/Resources/
 
-#Only for custom agent
-if [ ! "$RELEASE" == 1 ]; then
-	echo 'creating package-root for building .pkg under'
-	mkdir -p ./package-root/Applications
-	
-	echo 'copying .app to package-root'
-	sudo cp -R $OCSNG_PATH ./package-root/Applications/
+if [ ! -d ./installer_gui/iceberg/plugins ]; then
+	mkdir ./installer_gui/iceberg/plugins/
+fi
 
-	echo 'setting default permissions on ./package-root/Applications'
-	sudo chown root:admin ./package-root/Applications
-	sudo chmod 775 ./package-root/Applications
+cd ./installer_gui/ocs_agent_config/
+xcodebuild -alltargets
+cp -R ./build/Release/ocs_agent_config.bundle ../iceberg/plugins/
+xcodebuild clean
+cd ../ocs_agent_daemon_options/
+xcodebuild -alltargets
+cp -R ./build/Release/ocs_agent_daemon_options.bundle ../iceberg/plugins/
+xcodebuild clean
+cd ../../
+cp ./scripts/installer.sh installer_gui/iceberg/scripts/
 
-	# package maker might spit out some permissions errors if the app or it's folders are on your system already, this is usually OK, read them to make sure
-	echo "building package"
-	sudo rm -R -f ./OCSNG.pkg
-	sudo /Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -build -proj OCSNG.pmproj -p ./OCSNG.pkg
+echo "moving $OCSNG_PATH...enter your password if needed"	
+sudo mv $OCSNG_PATH ./installer_gui/iceberg/
 
-	FILES="ocsinventory-agent README INSTALL launchfiles OCSNG.pkg scripts ocsinventory-agent.cfg modules.conf cacert.pem"
-
-	mkdir $FINAL_PKG_NAME
-	cp -R $FILES $FINAL_PKG_NAME/
-	zip -r $FINAL_PKG_NAME $FINAL_PKG_NAME/ -x \*CVS\* -x \*svn\*
-	rm -R -f $FINAL_PKG_NAME
-
-else
-	if [ ! -d ./installer_gui/iceberg/plugins ]; then
-		mkdir ./installer_gui/iceberg/plugins/
-	fi
-
-	cd ./installer_gui/ocs_agent_config/
-	xcodebuild -alltargets
-	cp -R ./build/Release/ocs_agent_config.bundle ../iceberg/plugins/
-	xcodebuild clean
-	cd ../ocs_agent_daemon_options/
-	xcodebuild -alltargets
-	cp -R ./build/Release/ocs_agent_daemon_options.bundle ../iceberg/plugins/
-	xcodebuild clean
-	cd ../../
-	cp ./scripts/installer.sh installer_gui/iceberg/scripts/
-
-	echo "moving $OCSNG_PATH...enter your password if needed"	
-	sudo mv $OCSNG_PATH ./installer_gui/iceberg/
-
-	echo 'Now you can build final gui installer using iceberg'
-fi 
+echo 'Now you can build final gui installer using iceberg'
 
 echo "done"
