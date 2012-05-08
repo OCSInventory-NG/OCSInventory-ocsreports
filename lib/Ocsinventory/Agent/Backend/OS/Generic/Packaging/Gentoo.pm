4973551f1e70bbab0503c1b2cfcf340af08c0164
frank <frank.bourdeau@ocsinventory-ng.org>
2012-05-08 15:00:04 +0200

    Fix bug #873377 for Gentoo

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/Gentoo.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/Gentoo.pm
@@ -10,14 +10,31 @@ sub run {
   my $common = $params->{common};
 
 # TODO: This had been rewrite from the Linux agent _WITHOUT_ being checked!
-  foreach (`equery list -i`){
-    if (/^([a-z]\w+-\w+\/\.*)-([0-9]+.*)/) {
-      $common->addSoftware({
-	  'NAME'          => $1,
-	  'VERSION'       => $2,
-	  });
-    }
-  }
+# New format for listing softwares with equery command (equery 0.3.0.5)
+# Older version don't run with these options
+
+  my $equery_vers = `equery --version | awk '{print $2}'`;
+
+  if ($equery_vers =~ /0.3/) {
+  	foreach (`equery list --format='\$cp \$fullversion' '*'`){
+    		if (/^(.*) (.*)/) {
+      			$common->addSoftware({
+	  		'NAME'          => $1,
+	  		'VERSION'       => $2,
+	  		});
+    		}
+  	}
+  } else {
+	# Old version of Gentoo
+	 foreach (`equery list -i`){
+    		if (/^([a-z]\w+-\w+\/\.*)-([0-9]+.*)/) {
+      			$common->addSoftware({
+          		'NAME'          => $1,
+          		'VERSION'       => $2,
+          		});
+		}
+    	}
+  } 
 }
 
 1;
