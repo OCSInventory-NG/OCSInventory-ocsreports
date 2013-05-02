95bd775dd265295aac2be357ffd6dd7fc64e9656
frank <frank.bourdeau@ocsinventory-ng.org>
2013-05-03 00:53:40 +0200

    Retrieve video memory size

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Lspci/Videos.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Lspci/Videos.pm
@@ -1,20 +1,29 @@
 package Ocsinventory::Agent::Backend::OS::Generic::Lspci::Videos;
 use strict;
 
+my $memory;
+
 sub run {
-  my $params = shift;
-  my $common = $params->{common};
+  	my $params = shift;
+  	my $common = $params->{common};
 
-  foreach(`lspci`){
+	foreach(`lspci`){
 
-    if(/graphics|vga|video/i && /^\S+\s([^:]+):\s*(.+?)(?:\(([^()]+)\))?$/i){
-
-      $common->addVideo({
-	  'CHIPSET'  => $1,
-	  'NAME'     => $2,
-	});
-
-    }
-  }
+    	if(/graphics|vga|video/i && /\d\d:\d\d.\d\s([^:]+):\s*(.+?)(?:\(([^()]+)\))?$/i){
+	  	my $slot = $1;
+		if (defined $slot) {
+			my @detail = `lspci -v -s $slot`;
+			foreach my $m (@detail) {	
+				if ($m =~/(.*)Memory(.*=\((\d*)-bit,\sprefetchable\)\s(.*)(\d*)M\]/i) {
+					$memory = $1;
+					$common->addVideo({
+	  					'CHIPSET'  => $1,
+	  					'NAME'     => $2,
+						'MEMORY'   => $memory,
+					});
+				}
+			}
+    	}
+  	}
 }
 1
