fce31bb06fab52ee8c125c14a126eaeda423f1e1
mortheres <guillaume.protet@mortheres.info>
2013-11-17 19:26:18 +0100

    Fix Bug #1216935 about Mac user agent not generating UUID

--- lib/Ocsinventory/Agent/Backend/OS/MacOS.pm
+++ lib/Ocsinventory/Agent/Backend/OS/MacOS.pm
@@ -21,38 +21,48 @@ sub run {
         my $OSName;
         my $OSComment;
         my $OSVersion;
+	my $uuid;
 		
-		# if we can load the system profiler, gather the information from that
-		if(can_load("Mac::SysProfile")){
-			my $profile = Mac::SysProfile->new();
-			my $data = $profile->gettype('SPSoftwareDataType');
-			return(undef) unless(ref($data) eq 'ARRAY');
-			
-			my $h = $data->[0];
-			
-			my $SystemVersion = $h->{'os_version'};
-			if ($SystemVersion =~ /^(.*?)\s+(\d+.*)/) {
-			    $OSName=$1;
-			    $OSVersion=$2;
-			} else {
-			    # Default values
-			    $OSName="Mac OS X";
-			    $OSVersion="Unknown";
-			}
-
+	# if we can load the system profiler, gather the information from that
+	if(can_load("Mac::SysProfile")){
+		my $profile = Mac::SysProfile->new();
+		my $data = $profile->gettype('SPSoftwareDataType');
+		return(undef) unless(ref($data) eq 'ARRAY');
+		
+		my $h = $data->[0];
+		
+		my $SystemVersion = $h->{'os_version'};
+		if ($SystemVersion =~ /^(.*?)\s+(\d+.*)/) {
+		    $OSName=$1;
+		    $OSVersion=$2;
 		} else {
-			# we can't load the system profiler, use the basic BSD stype information
-			# Operating system informations
-			chomp($OSName=`uname -s`);
-			chomp($OSVersion=`uname -r`);			
+		    # Default values
+		    $OSName="Mac OS X";
+		    $OSVersion="Unknown";
 		}
+
+	} else {
+		# we can't load the system profiler, use the basic BSD stype information
+		# Operating system informations
+		chomp($OSName=`uname -s`);
+		chomp($OSVersion=`uname -r`);			
+	}
 		
-		# add the uname -v as the comment, not really needed, but extra info never hurt
-		chomp($OSComment=`uname -v`);
+	# add the uname -v as the comment, not really needed, but extra info never hurt
+	chomp($OSComment=`uname -v`);
+
+	# We get UUID
+	if(can_run("ioreg")) {
+		my @ioreg = split ("=", `ioreg -rd1 -c IOPlatformExpertDevice | grep -E '(UUID)'`); 
+		chomp($uuid = $ioreg[1]);
+		$uuid =~ s/\"|\s//g;
+	}
+
         $common->setHardware({
                 OSNAME		=> $OSName,
                 OSCOMMENTS	=> $OSComment,
                 OSVERSION	=> $OSVersion,
+		UUID		=> $uuid,
         });
 }
 
