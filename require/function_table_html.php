6cd7452db5dfdb475bbdd4fd349f0b89604de598
Alibert Mickael <mickael.alibert@factorfx.com>
2014-05-07 18:20:08 +0200

    Table CSS  Fix

--- require/function_table_html.php
+++ require/function_table_html.php
@@ -414,6 +414,8 @@ function xml_decode( $txt ) {
     	        			"sSortDescending": ": <?php echo $l->g(1348); ?>",
     	        		}
 				 },
+				 "scrollX": "100%",
+				 "scrollCollapse":true,
 				
        		});
 			$("body").on("click","#disp"+table_name,function(){
@@ -456,7 +458,7 @@ function xml_decode( $txt ) {
 	<?php
 	if ($titre != "")
 		printEnTete_tab($titre);
-	echo "<br><table id='".$option['table_name']."' class='table table-striped table-bordered table-condensed table-hover'><thead><tr>";
+	echo "<br><div class='tableContainer'><table id='".$option['table_name']."' class='table table-striped table-bordered table-condensed table-hover'><thead><tr>";
 		//titre du tableau
 	foreach($columns as $k=>$v)
 	{		
@@ -468,21 +470,9 @@ function xml_decode( $txt ) {
 		}	
 	}
 	echo "</tr>
-    </thead>
-		<tfoot>";
-	foreach($columns as $k=>$v)
-	{
-		if(array_key_exists($k,$lbl_column)){
-			echo "<th><font >".$lbl_column[$k]."</font></th>";
-		}
-		else{
-			echo "<th><font >".$k."</font></th>";
-		}
-	}
-	echo "</tr>
-	</tfoot>";
+    </thead>";
 			
-    echo "</table></div>";
+    echo "</table></div></div>";
 	echo "<input type='hidden' id='SUP_PROF' name='SUP_PROF' value=''>";
 	echo "<input type='hidden' id='MODIF' name='MODIF' value=''>";
 	echo "<input type='hidden' id='SELECT' name='SELECT' value=''>";
