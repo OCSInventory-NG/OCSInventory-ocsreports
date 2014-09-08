099d8ebe097593c0477ff94699a67f9a975faf51
Arthur JAOUEN <arthur@arthur-factorfx.factorfx.com>
2014-09-08 14:12:04 +0200

    Fix bug on install

--- install.php
+++ install.php
@@ -102,10 +102,7 @@ $nberr = 0;
 
 if( isset($_POST["name"])) {
 		$link=dbconnect($_POST["host"],$_POST["name"],$_POST["pass"],$_POST["database"]);
-		if ($link == 'NO_DATABASE') {
-			$firstAttempt=false;
-			msg_error($l->g(2001)." ".$l->g(2115));
-		} else if(mysqli_connect_errno()) {
+		if(mysqli_connect_errno()) {
 			$firstAttempt=false;
 			msg_error($l->g(2001)." ".$l->g(249).
 							" (".$l->g(2010)."=".$_POST["host"].
