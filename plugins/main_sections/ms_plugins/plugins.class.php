5fb1c18e4df2978060dbcdeea403547db140f1f2
gillesdubois <gillesdubois18@gmail.com>
2015-05-12 16:26:03 +0200

    Add add_menu / add_sub_menu and sql_query functions

--- plugins/main_sections/ms_plugins/plugins.class.php
+++ plugins/main_sections/ms_plugins/plugins.class.php
@@ -27,16 +27,111 @@ class plugins{
 		$this->rights = $this->rights + $table;
 	}
 	
+	/**
+	 * This function create a menu (Not a sub-menu) in OCS inventory.
+	 * As default, only super administrator profile can see the created menu.
+	 * 
+	 * @param string $name : The name of the menu you want to crate
+	 * @param integer $label : You need to give a label to your menu, it's like a reference for OCS. 
+	 */
 	function add_menu($name, $label){
-		echo "Création d'un menu !";
+		
+		$xmlfile = OCS_BASE_DIR."config/main_menu.xml";
+
+		if (file_exists($xmlfile)){
+			$xml = simplexml_load_file($xmlfile);
+		}
+		
+		$menu = $xml->addChild("menu-elem");
+		$menu->addAttribute("id","ms_".$name."");
+		$menu->addChild("label","g(".$label.")");
+		$menu->addChild("url","ms_".$name."");
+		
+		$xml->asXML($xmlfile);
+		
+		$xmlfile = OCS_BASE_DIR."config/urls.xml";
+				
+		if (file_exists($xmlfile)){
+			$xml = simplexml_load_file($xmlfile);
+		}
+		
+		$urls = $xml->addChild("url");
+		$urls->addAttribute("key","ms_".$name."");
+		$urls->addChild("value",$name);
+		$urls->addChild("directory","ms_".$name."");
+		
+		$xml->asXML($xmlfile);
+		
+		$xmlfile = OCS_BASE_DIR."config/profiles/sadmin.xml";
+		
+		if (file_exists($xmlfile)){
+			$xml = simplexml_load_file($xmlfile);
+		}
+		
+		$xml->pages->addChild("page","ms_".$name."");
+		
+		$xml->asXML($xmlfile);
+		
+		$file = fopen(OCS_BASE_DIR."plugins/language/english/english.txt", "a+");
+		fwrite($file, $label." ".$name);
+		fclose($file);
+		
 	}
 	
 	function del_menu($name, $label){
 		
 	}
 	
+	/**
+	 * This function create a sub-menu in OCS Iventory
+	 * As default, only super administrator profile can see the created menu.
+	 * 
+	 * @param string $name : The name of the menu you want to crate
+	 * @param string $menu : The name of the main menu (see documentation)
+	 * @param integer $label : You need to give a label to your menu, it's like a reference for OCS. 
+	 */
 	function add_sub_menu($name, $menu, $label){
-		echo "Création d'un sous menu !";
+		
+		$xmlfile = OCS_BASE_DIR."config/main_menu.xml";
+		
+		if (file_exists($xmlfile)){
+			$xml = simplexml_load_file($xmlfile);
+		}
+		
+		$menu = $xml->addChild("menu-elem");
+		$menu->addAttribute("id","ms_".$name."");
+		$menu->addChild("label","g(".$label.")");
+		$menu->addChild("url","ms_".$name."");
+		
+		$xml->asXML($xmlfile);
+		
+		$xmlfile = OCS_BASE_DIR."config/urls.xml";
+		
+		if (file_exists($xmlfile)){
+			$xml = simplexml_load_file($xmlfile);
+		}
+		
+		$urls = $xml->addChild("url");
+		$urls->addAttribute("key","ms_".$name."");
+		$urls->addChild("value",$name);
+		$urls->addChild("directory","ms_".$name."");
+		
+		$xml->asXML($xmlfile);
+		
+		$xmlfile = OCS_BASE_DIR."config/profiles/sadmin.xml";
+		
+		if (file_exists($xmlfile)){
+			$xml = simplexml_load_file($xmlfile);
+		}
+		
+		$xml->pages->addChild("page","ms_".$name."");
+		
+		$xml->asXML($xmlfile);
+		
+		$file = fopen(OCS_BASE_DIR."plugins/language/english/english.txt", "a+");
+		fwrite($file, $label." ".$name);
+		fclose($file);
+		
 	}
 
 	function del_sub_menu($name, $menu, $label){
@@ -51,8 +146,20 @@ class plugins{
 		
 	}
 	
+	/**
+	 * This function try to execute your query and throw an error message if this is a problems in the query.
+	 * 
+	 * @param string $query : Your database query here !
+	 */
 	function sql_query($query){
-		echo "Requete SQL";
+		try {
+		    $dbh = new PDO('mysql:host=localhost;dbname=ocsweb', 'ocs', 'ocs');
+		    $dbh->query($query);
+		    $dbh = null;
+		} catch (PDOException $e) {
+		    print "Error !: " . $e->getMessage() . "<br/>";
+		    die();
+		}
 	}
 	
 }
