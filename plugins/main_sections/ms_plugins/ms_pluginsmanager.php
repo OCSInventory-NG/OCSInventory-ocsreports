e347f23accb59c99e80bab5761b5b063e9596a76
gillesdubois <gillesdubois18@gmail.com>
2015-05-21 17:41:03 +0200

    Add plugin datatable where you can remove and check for installed plugins

--- plugins/main_sections/ms_plugins/ms_pluginsmanager.php
+++ plugins/main_sections/ms_plugins/ms_pluginsmanager.php
@@ -1,14 +1,4 @@
 <?php
-//====================================================================================
-// OCS INVENTORY REPORTS
-// Copyleft Erwan GOALOU 2010 (erwan(at)ocsinventory-ng(pt)org)
-// Web: http://www.ocsinventory-ng.org
-//
-// This code is open source and may be copied and modified as long as the source
-// code is always made freely available.
-// Please refer to the General Public Licence http://www.gnu.org/ or Licence.txt
-//====================================================================================
-
 
 if(AJAX){
 	parse_str($protectedPost['ocs']['0'], $params);
@@ -19,13 +9,59 @@ if(AJAX){
 else{
 	$ajax=false;
 }
-require('require/function_stats.php');
+
+require ('pluginsdelete.php');
 
 printEnTete($l->g(6000));
 
+if ($protectedPost['SUP_PROF'] != ''){
+	delete_plugin($protectedPost['SUP_PROF']);
+	$tab_options['CACHE']='RESET';
+}
 
+$form_name="show_all_plugins";
+$table_name=$form_name;
+$tab_options=$protectedPost;
+$tab_options['form_name']=$form_name;
+$tab_options['table_name']=$table_name;
+
+echo open_form($form_name);
+$list_fields=array('ID'=>'id',
+				   $l->g(7002)=>'name',
+				   $l->g(7003)=>'version',
+				   $l->g(7004)=>'licence',
+				   $l->g(7005)=>'author',
+				   'Required OCS ver.'=>'verminocs',
+				   'Activated'=>'activated',
+				   $l->g(7006) =>'reg_date'
+				);			
+
+$tab_options['FILTRE']=array_flip($list_fields);
+$tab_options['FILTRE']['NAME']=$l->g(49);
+asort($tab_options['FILTRE']); 
+$list_fields['SUP']='ID';
+$list_fields['CHECK']='ID';
+
+$list_col_cant_del=array('SUP'=>'SUP','CHECK'=>'CHECK');
+$default_fields= array($l->g(7002)=>$l->g(7002),$l->g(7003)=>$l->g(7003),$l->g(7004)=>$l->g(7005),$l->g(7006),$l->g(7006));
+$sql=prepare_sql_tab($list_fields,$list_col_cant_del);
+$tab_options['ARG_SQL']=$sql['ARG'];
+$queryDetails  = $sql['SQL'].",ID from plugins";
+$tab_options['LBL_POPUP']['SUP']=$l->g(7007)." ";
+$tab_options['LBL']['SUP']=$l->g(122);
+
+$tab_options['LIEN_LBL']['NAME']='index.php?'.PAG_INDEX.'='.$pages_refs['ms_pluginsmanager'].'&head=1&id=';
+$tab_options['LIEN_CHAMP']['NAME']='ID';
+$tab_options['LBL']['NAME']=$l->g(49);
+
+ajaxtab_entete_fixe($list_fields,$default_fields,$tab_options,$list_col_cant_del);
+$img['image/delete.png']=$l->g(162);
+del_selection($form_name);
+echo close_form();
 
 if ($ajax){
 	ob_end_clean();
+	tab_req($list_fields,$default_fields,$list_col_cant_del,$queryDetails,$tab_options);
 }
 ?>
+
