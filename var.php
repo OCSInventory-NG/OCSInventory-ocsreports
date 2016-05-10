523441d7ba4fb61da4ba674ba96d67ef5a9adc96
Gilles Dubois <gillesdubois@users.noreply.github.com>
2016-05-10 17:04:32 +0200

    Update var.php and make it more easier to read #88

--- var.php
+++ var.php
@@ -13,38 +13,64 @@
 define('DOCUMENT_REAL_ROOT', dirname(__FILE__));
 define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
 
-define("GUI_VER", "7009");												// Version of the database
-define("GUI_VER_SHOW","2.2");        								    // Version of the GUI
-define("MAC_FILE", __DIR__ . '/files/oui.txt');									// File containing MAC database
-define("DEFAULT_LANGUAGE","english");    								// Default language
-define("PAG_INDEX","function");         								// define name in url (like multi=32)
-define("DEMO",false);			        								// Define if we use demo version or not (for OCS TEAM, other=> DO NOT USE IT)
-define("DEMO_LOGIN",'demo');											// Define demo login for connexion
-define("DEMO_PASSWD",'demo');											// Define demo password for connexion
-define("DFT_DB_CMPT",'ocs');  											// Define default login to connect to database
-define("DFT_DB_PSWD",'ocs');											// Define default password to connect to database
-define("DFT_GUI_CMPT",'admin');											// Define default login to connect to GUI
-define("DFT_GUI_PSWD",'admin');											// Define default password to connect to GUI
-define('BACKEND',__DIR__ . '/backend/');									   	 	// Define backend Directory
-define('PHPCAS', __DIR__ . '/libraries/phpcas/CAS.php');				// Path to CAS (change to use system provided library)
-define('TC_LIB_BARCODE',  __DIR__ . '/libraries/tclib/Barcode/autoload.php'); // Path to tc-lib-barcode library, change to use system library
-define('PASSWORD_COMPAT', __DIR__ . '/libraries/password_compat/password.php'); // Path to tc-lib-barcode library, change to use system library
-define('PLUGINS_DL_DIR', __DIR__ . '/download/');
-define('PLUGINS_SRV_SIDE', __DIR__ . '/upload/');
-define('CONFIG_DIR',__DIR__ . '/config/');
-define('CD_CONFIG_DIR', CONFIG_DIR."computer/" );
-define('PLUGINS_DIR',__DIR__ . '/plugins/');										// Define plugins Directory
-define('PLUGINS_GUI_DIR','/tmp/');
-define('HEADER_HTML',__DIR__ . '/require/html_header.php');						// Define html_header file
-define('FOOTER_HTML',__DIR__ . '/require/footer.php');								// Define footer file
-define('MAIN_SECTIONS_DIR',"plugins/main_sections/");				//
-define('DEV_OPTION',false);												// Define DEV Options DO NOT USE
-define('PC4PAGE',20);													// Define result by page MUST in (5,10,15,20,50,100,200,1000000);
-define('CSRF',1000);														// max number of csrf session
-define('ETC_DIR', DOCUMENT_REAL_ROOT);									// Configuration dir
-define('VARLIB_DIR', DOCUMENT_ROOT);									// Data default dir
-define('VARLOG_DIR', DOCUMENT_ROOT);									// Log defaut dir
-define('CONF_MYSQL', ETC_DIR.'/dbconfig.inc.php');						// Define dbconf file
+//====================================================================================
+// Default configuration dir (Logs / Data / Document root / Sql config file)
+//====================================================================================
+define('ETC_DIR', DOCUMENT_REAL_ROOT);  // Ocsreports root folder ( /usr/share/ocsinventory-reports/ocsreports by default)						
+define('VARLIB_DIR', "/var/lib/ocsinventory-reports");	// Default lib dir
+define('VARLOG_DIR', "/var/lib/ocsinventory-reports");	// Défault log dir
+define('CONF_MYSQL', ETC_DIR.'/dbconfig.inc.php'); // Database configuration infos
 
+//====================================================================================
+// Librairies / Backend / Mac address file
+//====================================================================================
+define("MAC_FILE", __DIR__ .'/files/oui.txt');	 /// Mac address file
+define('BACKEND', __DIR__ .'/backend/');    // Backend folder (AUTH / CAS / etc )
+define('PHPCAS', __DIR__ .'/libraries/phpcas/CAS.php');  // Lib
+define('TC_LIB_BARCODE',  __DIR__ .'/libraries/tclib/Barcode/autoload.php'); // Lib
+define('PASSWORD_COMPAT', __DIR__ .'/libraries/password_compat/password.php'); // Lib
+
+//====================================================================================
+// GUI Options
+//====================================================================================
+define("GUI_VER", "7009");	// ocs' mysql database version									
+define("GUI_VER_SHOW", "2.2");    // GUI Version
+define("DEFAULT_LANGUAGE", "english");   // Default GUI language
+define("PAG_INDEX", "function");     
+
+//====================================================================================
+// Default OCS DIR
+//====================================================================================
+define('CONFIG_DIR', __DIR__ .'/config/');  // Configuration dir
+define('CD_CONFIG_DIR', CONFIG_DIR."computer/" );  //Computer detail configuration dir
+define('PLUGINS_DIR', __DIR__ .'/plugins/'); // Plugins dir
+define('PLUGINS_GUI_DIR', '/tmp/');
+define('HEADER_HTML', __DIR__ .'/require/html_header.php'); // HEADER for ocsreports
+define('FOOTER_HTML', __DIR__ .'/require/footer.php');  // FOOTER for ocsreports
+define('MAIN_SECTIONS_DIR', "plugins/main_sections/"); // Main section dir
+define('DEV_OPTION', false);
+
+//====================================================================================
+// Plugins Configuration
+//====================================================================================
+define('PLUGINS_DL_DIR', __DIR__ .'/download/');  // Dir where you put plugin sources
+define('PLUGINS_SRV_SIDE', __DIR__ .'/upload/'); // Don't touch this dir used by plugin engine
+
+//====================================================================================
+// Misc Options
+//====================================================================================
+define('PC4PAGE', 20);
+define('CSRF', 1000);
+
+//====================================================================================
+// Demo mode config and defaults accounts logins
+//====================================================================================
+define("DEMO",false);
+define("DEMO_LOGIN",'demo');
+define("DEMO_PASSWD",'demo');
+define("DFT_DB_CMPT",'ocs');
+define("DFT_DB_PSWD",'ocs');
+define("DFT_GUI_CMPT",'admin');
+define("DFT_GUI_PSWD",'admin');
 
 ?>
