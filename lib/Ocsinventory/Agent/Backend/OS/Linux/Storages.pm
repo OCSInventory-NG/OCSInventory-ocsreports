846b69da048897dde2e06441e240b4b1aa2fff48
frankb <frank.bourdeau@ocsinventory-ng.org>
2016-05-12 09:26:30 +0200

    Updated capacity to always report sizes in MB

--- lib/Ocsinventory/Agent/Backend/OS/Linux/Storages.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Linux/Storages.pm
@@ -8,198 +8,189 @@ sub check {1}
 ######## TODO
 # Do not remove, used by other modules
 sub getFromUdev {
-  	my @devs;
+    my @devs;
 
-  	foreach (glob ("/dev/.udev/db/*")) {
-    	my ($scsi_coid, $scsi_chid, $scsi_unid, $scsi_lun, $path, $device, $vendor, $model, $revision, $serial, $serial_short, $type, $bus, $capacity);
-    	if (/^(\/dev\/.udev\/db\/.*)([sh]d[a-z]+)$/) {
-      		$path = $1;
-      		$device = $2;
-      		open (PATH, $1 . $2);
-      		while (<PATH>) {
-        		if (/^S:.*-scsi-(\d+):(\d+):(\d+):(\d+)/) {
-          			$scsi_coid = $1;
-          			$scsi_chid = $2;
-          			$scsi_unid = $3;
-          			$scsi_lun = $4;
-        		}
-        		$vendor = $1 if /^E:ID_VENDOR=(.*)/; 
-        		$model = $1 if /^E:ID_MODEL=(.*)/; 
-        		$revision = $1 if /^E:ID_REVISION=(.*)/;
-        		$serial = $1 if /^E:ID_SERIAL=(.*)/;
-        		$serial_short = $1 if /^E:ID_SERIAL_SHORT=(.*)/;
-        		$type = $1 if /^E:ID_TYPE=(.*)/;
-        		$bus = $1 if /^E:ID_BUS=(.*)/;
-      		}
-      		$serial_short = $serial unless $serial_short =~ /\S/;
-      		$capacity = getCapacity($device);
-      		push (@devs, {NAME => $device, MANUFACTURER => $vendor, MODEL => $model, DESCRIPTION => $bus, TYPE => $type, DISKSIZE => $capacity, SERIALNUMBER => $serial_short, FIRMWARE => $revision, SCSI_COID => $scsi_coid, SCSI_CHID => $scsi_chid, SCSI_UNID => $scsi_unid, SCSI_LUN => $scsi_lun});
-      		close (PATH);
-    	}
-  	}
-
-  	return @devs;
+    foreach (glob ("/dev/.udev/db/*")) {
+        my ($scsi_coid, $scsi_chid, $scsi_unid, $scsi_lun, $path, $device, $vendor, $model, $revision, $serial, $serial_short, $type, $bus, $capacity);
+        if (/^(\/dev\/.udev\/db\/.*)([sh]d[a-z]+)$/) {
+            $path = $1;
+            $device = $2;
+            open (PATH, $1 . $2);
+            while (<PATH>) {
+                if (/^S:.*-scsi-(\d+):(\d+):(\d+):(\d+)/) {
+                    $scsi_coid = $1;
+                    $scsi_chid = $2;
+                    $scsi_unid = $3;
+                    $scsi_lun = $4;
+                }
+                $vendor = $1 if /^E:ID_VENDOR=(.*)/; 
+                $model = $1 if /^E:ID_MODEL=(.*)/; 
+                $revision = $1 if /^E:ID_REVISION=(.*)/;
+                $serial = $1 if /^E:ID_SERIAL=(.*)/;
+                $serial_short = $1 if /^E:ID_SERIAL_SHORT=(.*)/;
+                $type = $1 if /^E:ID_TYPE=(.*)/;
+                $bus = $1 if /^E:ID_BUS=(.*)/;
+            }
+            $serial_short = $serial unless $serial_short =~ /\S/;
+            $capacity = getCapacity($device);
+            push (@devs, {NAME => $device, MANUFACTURER => $vendor, MODEL => $model, DESCRIPTION => $bus, TYPE => $type, DISKSIZE => $capacity, SERIALNUMBER => $serial_short, FIRMWARE => $revision, SCSI_COID => $scsi_coid, SCSI_CHID => $scsi_chid, SCSI_UNID => $scsi_unid, SCSI_LUN => $scsi_lun});
+            close (PATH);
+        }
+    }
+    return @devs;
 }
-
 
 sub getFromSysProc {
-	my($dev, $file) = @_;
-  	my $value;
+    my($dev, $file) = @_;
+    my $value;
 
-  	foreach ("/sys/block/$dev/device/$file", "/proc/ide/$dev/$file") {
-    	next unless open PATH, $_;
-    	chomp(my $value = <PATH>);
-    	$value =~ s/^(\w+)\W*/$1/;
-    	return $value;
-  	}
+    foreach ("/sys/block/$dev/device/$file", "/proc/ide/$dev/$file") {
+        next unless open PATH, $_;
+        chomp(my $value = <PATH>);
+        $value =~ s/^(\w+)\W*/$1/;
+        return $value;
+    }
 }
 
-
 sub getCapacity {
-	my ($dev) = @_;
-  	my $cap;
-  	chomp ($cap = `fdisk -s /dev/$dev 2>/dev/null`); #requires permissions on /dev/$dev
-  	$cap = int ($cap/1000) if $cap;
-  	return $cap;
+    my ($dev) = @_;
+    my $cap;
+    chomp ($cap = `fdisk -s /dev/$dev 2>/dev/null`); #requires permissions on /dev/$dev
+    $cap = int ($cap/1024) if $cap;
+    return $cap;
 }
 
 sub getDescription {
-  	my ($name, $manufacturer, $description, $serialnumber) = @_;
-	# detected as USB by udev
-	# TODO maybe we should trust udev detection by default?
-  	return "USB" if (defined ($description) && $description =~ /usb/i);
+    my ($name, $manufacturer, $description, $serialnumber) = @_;
+    # detected as USB by udev
+    # TODO maybe we should trust udev detection by default?
+    return "USB" if (defined ($description) && $description =~ /usb/i);
 
-  	if ($name =~ /^s/) { # /dev/sd* are SCSI _OR_ SATA
-    	if ($manufacturer =~ /ATA/ || $serialnumber =~ /ATA/ || $description =~ /ATA/i) {
-      	return  "SATA";
-    	} else {
-      		return "SCSI";
-    	}
-  	} else {
-    	return "IDE";
-  	}
+    if ($name =~ /^s/) { # /dev/sd* are SCSI _OR_ SATA
+        if ($manufacturer =~ /ATA/ || $serialnumber =~ /ATA/ || $description =~ /ATA/i) {
+            return  "SATA";
+        } else {
+            return "SCSI";
+        }
+    } else {
+        return "IDE";
+    }
 }
 
 sub getManufacturer {
-  	my ($model) = @_;
-  	#if($model =~ /(maxtor|western|sony|compaq|hewlett packard|ibm|seagate|toshiba|fujitsu|lg|samsung|nec|transcend)/i) {
-    #	return ucfirst(lc($1));
+    my ($model) = @_;
+    #if($model =~ /(maxtor|western|sony|compaq|hewlett packard|ibm|seagate|toshiba|fujitsu|lg|samsung|nec|transcend)/i) {
+    #    return ucfirst(lc($1));
     if ($model =~ /^(IBM|LG|NEC)/){
         return $1;
     } elsif ($model =~ /(maxtor|western|sony|compaq|hewlett packard|ibm|seagate|toshiba|fujitsu|lg|samsung|nec|transcend)/i) {
         $model = lc($1);
         $model = s/\b(\w)/\u$1/g;
         return $model;
-  	} elsif ($model =~ /^HP/) {
-    	return "Hewlett-Packard";
-  	} elsif ($model =~ /^WD/) {
-    	return "Western Digital";
-  	} elsif ($model =~ /^ST/) {
-    	return "Seagate";
-  	} elsif ($model =~ /^(HD|IC|HU)/) {
-    	return "Hitachi";
-  	}
+    } elsif ($model =~ /^HP/) {
+        return "Hewlett-Packard";
+    } elsif ($model =~ /^WD/) {
+        return "Western Digital";
+    } elsif ($model =~ /^ST/) {
+        return "Seagate";
+    } elsif ($model =~ /^(HD|IC|HU)/) {
+        return "Hitachi";
+    }
 }
 
 # some hdparm release generated kernel error if they are
 # run on CDROM device
 # http://forums.ocsinventory-ng.org/viewtopic.php?pid=20810
 sub correctHdparmAvailable {
-  	return unless can_run("hdparm");
-  	my $hdparmVersion = `hdparm -V`;
-  	if ($hdparmVersion =~ /^hdparm v(\d+)\.(\d+)(\.|$)/) {
-    	return 1 if $1>9;
-    	return 1 if $1==9 && $2>=15;
-  	}	
-  	return;
+    return unless can_run("hdparm");
+    my $hdparmVersion = `hdparm -V`;
+    if ($hdparmVersion =~ /^hdparm v(\d+)\.(\d+)(\.|$)/) {
+        return 1 if $1>9;
+        return 1 if $1==9 && $2>=15;
+    }    
+    return;
 }
 
-
 sub run {
-	my $params = shift;
-  	my $logger = $params->{logger};
-  	my $common = $params->{common};
+    my $params = shift;
+    my $logger = $params->{logger};
+    my $common = $params->{common};
+    my $devices = {};
+    my ($serial,$cap,$unit,$model,$manufacturer,$type,$desc,$firmware,$name);
+    my @partitions;
 
-  	my $devices = {};
+    # Get complementary information in hash tab
+    if (can_run ("lshal")) {
+        my %temp;
+        my $in = 0;
+        my $value;
+        foreach my $line (`lshal`) {
+            chomp $line;
+            if ($line =~ s{^udi = '/org/freedesktop/Hal/devices/storage.*}{}) {
+                $in = 1;
+                %temp = ();
+            } elsif ($in == 1 and $line =~ s{^\s+(\S+) = (.*) \s*\((int|string|bool|string list|uint64)\)}{} ) {
+                my $key = $1;
+                my $value = $2;
+                $value =~ s/^'(.*)'\s*$/$1/; # Drop the quote
+                $value =~ s/\s+$//; # Drop the trailing white space
+                if ($key eq 'storage.serial') {
+                      $temp{SERIALNUMBER} = $value;
+                } elsif ($key eq 'storage.firmware_version') {
+                      $temp{FIRMWARE} = $value;
+                } elsif ($key eq 'block.device') {
+                      $value =~ s/\/dev\/(\S+)/$1/;
+                      $temp{NAME} = $value;
+                } elsif ($key eq 'info.vendor') {
+                      $temp{MANUFACTURER} = $value;
+                } elsif ($key eq 'storage.model') {
+                      $temp{MODEL} = $value;
+                } elsif ($key eq 'storage.drive_type') {
+                      $temp{TYPE} = $value;
+                } elsif ($key eq 'storage.size') {
+                      $temp{DISKSIZE} = int($value/(1024*1024) + 0.5);
+                }
+            } elsif ($in== 1 and $line eq '' and $temp{NAME}) {
+                $in = 0 ;
+                $devices->{$temp{NAME}} = {%temp};
+            }
+        }
+    }
 
-	my ($serial,$cap,$unit,$model,$manufacturer,$type,$desc,$firmware,$name);
-	my @partitions;
+    if (can_run("smartctl")){
+        open PARTINFO,'</proc/partitions' or warn;
+        foreach(<PARTINFO>){
+            if (/^\s*(\d*)\s*(\d*)\s*(\d*)\s*([sh]d[a-z]+)$/i){
+                push(@partitions,$4);
+            }
+        }
 
-  	# Get complementary information in hash tab
-  	if (can_run ("lshal")) {
-    	my %temp;
-    	my $in = 0;
-    	my $value;
-    	foreach my $line (`lshal`) {
-      		chomp $line;
-      		if ( $line =~ s{^udi = '/org/freedesktop/Hal/devices/storage.*}{}) {
-        		$in = 1;
-        		%temp = ();
-      		} elsif ($in == 1 and $line =~ s{^\s+(\S+) = (.*) \s*\((int|string|bool|string list|uint64)\)}{} ) {
-        		my $key = $1;
-        		my $value = $2;
-        		$value =~ s/^'(.*)'\s*$/$1/; # Drop the quote
-        		$value =~ s/\s+$//; # Drop the trailing white space
-
-        		if ($key eq 'storage.serial') {
-          			$temp{SERIALNUMBER} = $value;
-        		} elsif ($key eq 'storage.firmware_version') {
-          			$temp{FIRMWARE} = $value;
-        		} elsif ($key eq 'block.device') {
-          			$value =~ s/\/dev\/(\S+)/$1/;
-          			$temp{NAME} = $value;
-        		} elsif ($key eq 'info.vendor') {
-          			$temp{MANUFACTURER} = $value;
-        		} elsif ($key eq 'storage.model') {
-          			$temp{MODEL} = $value;
-        		} elsif ($key eq 'storage.drive_type') {
-          			$temp{TYPE} = $value;
-        		} elsif ($key eq 'storage.size') {
-          			$temp{DISKSIZE} = int($value/(1024*1024) + 0.5);
-        		}
-      		} elsif ($in== 1 and $line eq '' and $temp{NAME}) {
-        		$in = 0 ;
-        		$devices->{$temp{NAME}} = {%temp};
-      		}
-		}
-  	}
-
-	if (can_run("smartctl")){
-		open PARTINFO,'</proc/partitions' or warn;
-	
-		foreach(<PARTINFO>){
-			if (/^\s*(\d*)\s*(\d*)\s*(\d*)\s*([sh]d[a-z]+)$/i){
-				push(@partitions,$4);
-			}
-		}
-
-		foreach my $dev (@partitions){
-			$name = "/dev/$dev";
-			my @sm = `smartctl -i $name`;
-		
-			for (@sm){
-				if (/^Model\sFamily:\s*(.*)/i){
-        			$desc = $1;
+        foreach my $dev (@partitions){
+            $name = "/dev/$dev";
+            my @sm = `smartctl -i $name`;
+            for (@sm){
+                if (/^Model\sFamily:\s*(.*)/i){
+                    $desc = $1;
                     if ($desc =~ /^(IBM|LG|NEC)/){
                         $manufacturer = $1;
-        			} elsif ($desc =~ /^(HP|Hewlett[ -]Packard)/) {
-            			$manufacturer="Hewlett-Packard";
-        			} elsif ($desc =~ /(maxtor|western digital|sony|compaq|seagate|toshiba|fujitsu|lg|samsung|nec|transcend)/i){
-            			$manufacturer = lc($1);
+                    } elsif ($desc =~ /^(HP|Hewlett[ -]Packard)/) {
+                        $manufacturer="Hewlett-Packard";
+                    } elsif ($desc =~ /(maxtor|western digital|sony|compaq|seagate|toshiba|fujitsu|lg|samsung|nec|transcend)/i){
+                        $manufacturer = lc($1);
                         $manufacturer =~ s/\b(\w)/\u$1/g;
-        			} elsif ($desc =~ /^WD/) {
-            			$manufacturer="Western Digital";
-        			} elsif ($desc =~ /^ST/) {
-            			$manufacturer="Seagate";
-        			} elsif ($desc =~ /^(HD|IC|HU)/) {
-            			$manufacturer="Hitachi";
-					} elsif ($desc =~ /^SandForce/) {
+                    } elsif ($desc =~ /^WD/) {
+                        $manufacturer="Western Digital";
+                    } elsif ($desc =~ /^ST/) {
+                        $manufacturer="Seagate";
+                    } elsif ($desc =~ /^(HD|IC|HU)/) {
+                        $manufacturer="Hitachi";
+                    } elsif ($desc =~ /^SandForce/) {
                         $manufacturer="Corsair";
                     }
-				}
+                }
 
-				if (/^Device\sModel:\s*(.*)/i){
-        			$model = $1;
+                if (/^Device\sModel:\s*(.*)/i){
+                    $model = $1;
                     if (!defined $manufacturer){
                         if ($model =~ /^(hitachi|toshiba|samsung)/) {
                            $manufacturer = lc($1);
@@ -210,42 +201,42 @@ sub run {
                            $manufacturer = "Hitachi";
                         }
                     }
-    			}
+                }
 
-    			if (/^Serial\sNumber:\s*(.*)/i){
-        			$serial = $1;	
-				}
+                if (/^Serial\sNumber:\s*(.*)/i){
+                    $serial = $1;
+                }
 
-				if (/^User\sCapacity:\s*(.*)\sbytes\s\[(.*)\s(.*)\]/i){
-					$cap = $1;
-					$unit = $3;
-					$cap =~ s/,//g;
-					if ($unit eq "MB") {
-						$cap = int($cap/1024);
-					} elsif ($unit eq "GB") {
-						$cap = int($cap/1048576);
-					} elsif ($unit eq "TB") {
-						$cap = int($cap/1073741824);
-					} else { 
-						$cap = undef; 
-					}
-				}
-				if (/^Firmware\sVersion:\s*(.*)/i){
-					$firmware = $1;
-				}
-			}
-			$common->addStorages({
-				DESCRIPTION => $desc,
-				DISKSIZE => $cap,
-				FIRMWARE => $firmware,
-				MANUFACTURER => $manufacturer,
-				MODEL => $model,
-				NAME => $name,
-				SERIALNUMBER => $serial,
-				TYPE => 'Disk',
-			});
-		}
-	}
+                if (/^User\sCapacity:\s*(.*)\sbytes\s\[(.*)\s(.*)\]/i){
+                    $cap = $1;
+                    $unit = $3;
+                    $cap =~ s/,//g;
+                    if ($unit eq "MB") {
+                        $cap = int($cap/1024);
+                    } elsif ($unit eq "GB") {
+                        $cap = int($cap/1048576);
+                    } elsif ($unit eq "TB") {
+                        $cap = int($cap/1073741824);
+                    } else { 
+                        $cap = undef; 
+                    }
+                }
+                if (/^Firmware\sVersion:\s*(.*)/i){
+                    $firmware = $1;
+                }
+            }
+            $common->addStorages({
+                DESCRIPTION => $desc,
+                DISKSIZE => $cap,
+                FIRMWARE => $firmware,
+                MANUFACTURER => $manufacturer,
+                MODEL => $model,
+                NAME => $name,
+                SERIALNUMBER => $serial,
+                TYPE => 'Disk',
+            });
+        }
+    }
 }
 
 1;
