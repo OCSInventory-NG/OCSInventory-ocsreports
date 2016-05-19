9d93b5476777b1bba8523f6589b0e215fed4557b
frankb <frank.bourdeau@ocsinventory-ng.org>
2016-05-19 17:02:50 +0200

    CPU inventory: bug correction, add new properties (LOGICAL_CPUS)

--- lib/Ocsinventory/Agent/Backend/OS/Linux/Archs/i386/CPU.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Linux/Archs/i386/CPU.pm
@@ -1,6 +1,9 @@
 package Ocsinventory::Agent::Backend::OS::Linux::Archs::i386::CPU;
 
 use strict;
+use warnings;
+use Data::Dumper;
+
 use Config;
 
 sub check { can_read("/proc/cpuinfo"); can_run("arch"); }
@@ -9,97 +12,71 @@ sub run {
 
     my $params = shift;
     my $common = $params->{common};
+    
     my @cpu;
+    my @cache;
     my $current;
     my $cpuarch = `arch`;
     chomp($cpuarch);
-    my $cpusocket;
-    my $siblings;
-    my $cpucores;
-    my $cpuspeed;
-    my $coreid;
+    my $datawidth;
+    my $index;
+    my $cpucount = 0;
+    my $l2cacheid;
+    my $l2cachesection;
+    my @dmiinfo;
 
-    $cpucores=0;
-    $siblings=0;
+    if ($cpuarch eq "x86_64"){
+        $datawidth = 64;
+    } else {
+        $datawidth = 32;
+    }
+    
     open CPUINFO, "</proc/cpuinfo" or warn;
-    foreach(<CPUINFO>) {
+    for (<CPUINFO>) {
         if (/^vendor_id\s*:\s*(Authentic|Genuine|)(.+)/i) {
-            $cpucores++;
             $current->{MANUFACTURER} = $2;
             $current->{MANUFACTURER} =~ s/(TMx86|TransmetaCPU)/Transmeta/;
             $current->{MANUFACTURER} =~ s/CyrixInstead/Cyrix/;
             $current->{MANUFACTURER} =~ s/CentaurHauls/VIA/;
         }
-
-        if (/^siblings\s*:\s*(\d+)/i){
-            $siblings++;
-        }
-        $current->{CURRENT_SPEED} = $1 if /^cpu\sMHz\s*:\s*(\d+)/i;
+        $current->{CORES} = $1 if /^cpu\scores\s*:\s*(\d+)/i;
+        $current->{LOGICAL_CPUS} = $1 if /^siblings\s*:\s*(\d+)/i;
+        $current->{SPEED} = $current->{CURRENT_SPEED} = $1 if /^cpu\sMHz\s*:\s*(\d+)/i;
         $current->{TYPE} = $1 if /^model\sname\s*:\s*(.+)/i;
-        $current->{L2CACHESIZE} = $1 if /^cache\ssize\s*:\s*(\d+)/i;
+        $current->{HPT} = 'yes' if /^flags\s*:.*\bht\b/i;
+        $current->{L2CACHESIZE}=$1 if (/^cache\ssize\s*:\s*(\d+)/i);
+        # if "cpu cores" or "siblings" are missing in /proc/cpuinfo (seen on several 1 vCPU configurations)
+        if ($current->{CORES} == 0) {
+            $current->{CORES} = 1;
+        }
+        if ($current->{LOGICAL_CPUS} == 0) {
+            $current->{LOGICAL_CPUS} = 1;
+        }
+        $index = $1 if ! defined $index && /^processor\s*:\s*(\d+)/i;
+        $index = $1 if /^physical\sid\s*:\s*(\d+)/i;
+        if (/^\s*$/) {
+            $current->{HPT} = 'no' if $current->{HPT} ne 'yes';
+            $current->{CPUARCH} = $cpuarch;
+            $current->{DATA_WIDTH} = $datawidth;
+            $current->{TYPE} =~ s/\s{2,}/ /g;
+        }
+        $current->{NBSOCKET}=$index+1;
     }
+    @dmiinfo=`dmidecode -t processor`;
+    for (@dmiinfo){
+        $current->{VOLTAGE}=$1 if (/Voltage:\s*(.*)V/i);
+        $current->{SOCKET} = $1 if (/Upgrade:\s*(.*)/i);
+        $current->{CPUSTATUS} = $2 if (/Status:\s*(.*),\s(.*)/i);
+    }
+    $cpu[$index] = $current;
+    $current = $index = undef;
 
-    # /proc/cpuinfo provides real time speed processor.
-    # Get optimal speed with dmidecode command
-    # Get also cpu cores with dmidecode command
-    # Get also voltage information with dmidecode command
-    @cpu = `dmidecode -t processor`;
-    $cpuspeed=0;
-    $cpusocket=0;
-    for (@cpu){
-        if (/Processor\sInformation/i){
-            $cpusocket++;
-            if ($cpuspeed != 0){
-                if ($cpusocket > $cpucores){
-                    last;
-                }
-            }
-            $cpuspeed=0;
-        }
-
-        if (/Current\sSpeed:\s*(.*) (|MHz|GHz)/i){
-            $cpuspeed = $1;
-            $current->{SPEED} = $cpuspeed;
-        }
-
-        if (/Core\sCount:\s*(\d+)/i){
-            $current->{CORES} = $1;
-        } else {
-            $current->{CORES} = $cpucores;
-        }
-        
-        # Is(Are) CPU(s) hyperthreaded?
-        if ($siblings == $current->{CORES}) {
-            # Hyperthreading is off
-            $current->{HPT}='on';
-        } else {
-            # Hyperthreading is on
-            $current->{HPT}='off';
-        }
-
-        if (/Voltage:\s*(.*)V/i){
-            $current->{VOLTAGE} = $1;
-        }
-
-        if (/Upgrade:\s*(.*)/i){
-            $current->{SOCKET} = $1;
-        }
-
-        $current->{CPUARCH}=$cpuarch;
-        if ($cpuarch eq "x86_64"){
-            $current->{DATA_WIDTH}=64;
-        } else {
-            $current->{DATA_WIDTH}=32;
-        }
-        
-        $current->{NBSOCKET} = $cpusocket;
-
-        # Add CPU when Populated
-        if (/Status:\s*(.*),\s(.*)/i){
-            $current->{CPUSTATUS} = $2;
-            $common->addCPU($current);
+    for my $c (@cpu) {
+        # sometimes "hardware id" are not contiguous (first hardware id = 0, second hardware id = 2)
+        if (defined $c) {
+            $common->addCPU($c);
         }
     }
 }
-
+    
 1;
