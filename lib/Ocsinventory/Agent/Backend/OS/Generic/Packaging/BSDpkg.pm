8ab39bc52b39111052d7b114c8d2d0c648218f74
kapouik <kapouik@troll-poilu.fr>
2016-12-17 21:23:21 +0100

    Change order for pkgng and pkgsrc
    On some FreeBSD, you can find old pkgsrc and pkgng after migrating from
    one to the other. It's better to ask in first pkgng.

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/BSDpkg.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/BSDpkg.pm
@@ -3,15 +3,15 @@ package Ocsinventory::Agent::Backend::OS::Generic::Packaging::BSDpkg;
 sub check {
     my $params = shift;
     my $common = $params->{common};
-    $common->can_run("pkg_info") || $common->can_run("pkg")
+    $common->can_run("pkg") || $common->can_run("pkg_info")
 }
 
 sub run {
     my $params = shift;
     my $common = $params->{common};
 
-    if ($common->can_run("pkg_info")) {
-        foreach(`pkg_info`){
+    if ($common->can_run("pkg")) {
+        foreach(`pkg info`){
             /^(\S+)-(\d+\S*)\s+(.*)/;
             my $name = $1;
             my $version = $2;
@@ -23,8 +23,8 @@ sub run {
                 'VERSION' => $version
             });
         }
-    } elsif ($common->can_run("pkg")) {
-        foreach(`pkg info`){
+    } elsif ($common->can_run("pkg_info")) {
+        foreach(`pkg_info`){
             /^(\S+)-(\d+\S*)\s+(.*)/;
             my $name = $1;
             my $version = $2;
