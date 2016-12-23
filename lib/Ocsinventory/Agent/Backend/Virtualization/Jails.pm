090e3cbd0e9365a54c7194052ee7aa5ec6e83ec6
kapouik <kapouik@troll-poilu.fr>
2016-12-24 00:02:35 +0100

    add support for multi ip in jail

--- lib/Ocsinventory/Agent/Backend/Virtualization/Jails.pm
+++ lib/Ocsinventory/Agent/Backend/Virtualization/Jails.pm
@@ -2,7 +2,7 @@ package Ocsinventory::Agent::Backend::Virtualization::Jails;
 
 use strict;
 
-sub check { 
+sub check {
     return(undef) unless -r '/usr/sbin/jls';
     return 1;
 }
@@ -12,7 +12,7 @@ my @jail;
 sub run {
     my $params = shift;
     my $common = $params->{common};
- 
+
     foreach my $line (`jls -h 2>/dev/null | sed -e "1d"`) {
         push @jail, $line;
     }
@@ -23,16 +23,41 @@ sub run {
         my $name=$jparam[7];
         my $subsystem=$jparam[9];
         my $status="running";
-        my $ipaddr=$jparam[36];
-		
-        $common->addVirtualMachine({
-            IPADDR => $ipaddr,
-            NAME => $name,
-            STATUS => $status,
-            SUBSYSTEM => "FreeBSD $subsystem",
-            VMID => $vmid,
-            VTYPE => "Jail",
-        });
+        my @ip;
+        my $ipv4=$jparam[39];
+        if ($ipv4 ne '-') {
+            my @ipv4=split(",",$ipv4);
+            foreach my $i (@ipv4) {
+                push @ip, $i;
+            }
+        }
+        my $ipv6=$jparam[41];
+        if ($ipv6 ne '-') {
+            my @ipv6=split(",",$ipv6);
+            foreach my $i (@ipv6) {
+                push @ip, $i;
+            }
+        }
+
+        my $ip=join "/", @ip;
+        if (defined $ip) {
+            $common->addVirtualMachine({
+                IPADDR => $ip,
+                NAME => $name,
+                STATUS => $status,
+                SUBSYSTEM => "FreeBSD $subsystem",
+                VMID => $vmid,
+                VTYPE => "Jail",
+            });
+        } else {
+            $common->addVirtualMachine({
+                NAME => $name,
+                STATUS => $status,
+                SUBSYSTEM => "FreeBSD $subsystem",
+                VMID => $vmid,
+                VTYPE => "Jail",
+            });
+        }
     }
 }
 
