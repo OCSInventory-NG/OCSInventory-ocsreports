6106584c3cd2ced76118bd7a0729478187911cd3
matt335672 <matthew.burt@nats.co.uk>
2017-07-20 09:16:45 +0100

    Add packager info from RPM Vendor field

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/RPM.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/RPM.pm
@@ -22,12 +22,12 @@ sub run {
     my @date;
     my @list;
     my $buff;
-    foreach (`rpm -qa --queryformat "%{NAME}.%{ARCH} %{VERSION}-%{RELEASE} --%{INSTALLTIME}-- --%{SIZE}-- %{SUMMARY}\n--\n" 2>/dev/null`) {
+    foreach (`rpm -qa --queryformat "%{NAME}.%{ARCH} %{VERSION}-%{RELEASE} --%{INSTALLTIME}-- --%{SIZE}-- --%{VENDOR}-- %{SUMMARY}\n--\n" 2>/dev/null`) {
         if (! /^--/) {
             chomp;
             $buff .= $_;
-        } elsif ($buff =~ s/^(\S+)\s+(\S+)\s+--(.*)--\s+--(.*)--\s+(.*)//) {
-            my ($name,$version,$installdate,$filesize,$comments) = ( $1,$2,$3,$4,$5 );
+        } elsif ($buff =~ s/^(\S+)\s+(\S+)\s+--(.*)--\s+--(.*)--\s+--(.*)--\s+(.*)//) {
+            my ($name,$version,$installdate,$filesize,$vendor,$comments) = ( $1,$2,$3,$4,$5,$6 );
             @date = localtime($installdate);
             $installdate = sprintf( "%04d-%02d-%02d %02d:%02d:%02d", $date[5] + 1900, $date[4] + 1, $date[3], $date[2], $date[1], $date[0]);
 
@@ -37,6 +37,7 @@ sub run {
                 'INSTALLDATE'   => $installdate,
                 'FILESIZE'      => $filesize,
                 'COMMENTS'      => $comments,
+                'PUBLISHER'     => $vendor,
                 'FROM'          => 'rpm'
             });
         } else {
