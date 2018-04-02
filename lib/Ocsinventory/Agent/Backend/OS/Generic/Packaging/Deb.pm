49cf9342430fc839b7d10892447d55f1ee88687c
fbomj <frank.bourdeau@ocsinventory-ng.org>
2018-04-02 15:44:52 +0200

    Add installation date in the inventory for Debian/ubuntu

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/Deb.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Packaging/Deb.pm
@@ -2,6 +2,9 @@ package Ocsinventory::Agent::Backend::OS::Generic::Packaging::Deb;
 
 use strict;
 use warnings;
+use File::Basename;
+use File::stat;
+
 
 sub check { 
     my $params = shift;
@@ -12,6 +15,21 @@ sub run {
     my $params = shift;
     my $common = $params->{common};
     my $size;
+    my $key;
+    my $value;
+    my %statinfo;
+
+    # List of files from which installation date will be extracted
+    my @listfile=glob('"/var/lib/dpkg/info/*.list"');
+
+    foreach my $file_list (@listfile){
+        my $stat=stat($file_list);
+        my ($year,$month,$day,$hour,$min,$sec)=(localtime($stat->mtime))[5,4,3,2,1,0];
+        $value=sprintf "%02d/%02d%02d %02d:%02d:%02d",($year+1900),$month,$day,$hour,$min,$sec;
+        $key=fileparse($file_list, ".list");
+        $key =~ s/(\s+):.+/$1/;
+        $statinfo{$key}=$value;
+    }
   
     # use dpkg-query --show --showformat='${Package}|||${Version}\n'
     foreach(`dpkg-query --show --showformat='\${Package}---\${Version}---\${Installed-Size}---\${Homepage}---\${Description}\n'`) {
@@ -21,14 +39,18 @@ sub run {
             } else {
                 $size='Unknown size';
             }
-            $common->addSoftware ({
-                'NAME'          => $1,
-                'VERSION'       => $2,
-                'FILESIZE'      => $size,
-                'PUBLISHER'     => $4,
-                'COMMENTS'      => $5,
-                'FROM'          => 'deb'
-            });
+            $key=$1;
+            if (exists $statinfo{$key}) {
+                $common->addSoftware ({
+                    'NAME'          => $1,
+                    'VERSION'       => $2,
+                    'FILESIZE'      => $size,
+                    'PUBLISHER'     => $4,
+                    'COMMENTS'      => $5,
+                    'INSTALLDATE'   => $statinfo{$key},
+                    'FROM'          => 'deb'
+                });
+            }
         }
     }
 }
