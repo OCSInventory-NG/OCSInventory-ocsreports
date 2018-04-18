a9a992cfb9eff89393cad1207d2ce2443c36e1dd
RudyLaurent <rudy.laurent@factorfx.com>
2018-04-18 09:58:02 +0200

    Fix for ubuntu and older Debian version

--- lib/Ocsinventory/Agent/Backend/OS/Generic/Repository/Debian.pm
+++ lib/Ocsinventory/Agent/Backend/OS/Generic/Repository/Debian.pm
@@ -15,14 +15,14 @@ sub run{
 
   my ($repo_name, $repo_baseurl);
 
-  my @repository=`apt-cache policy | grep -i http | awk {'print \$2 \$3'} | sort -u`;
+  my @repository=`apt-cache policy | grep -i http | awk {'print \$2 " " \$3'} | sort -u`;
   push @repository, "\n";
 
   for (my $i=0;$i<$#repository;$i++){
       my $line=$repository[$i];
 
-      $repo_name=$1 if ($line =~ /\/([^\/]*)$/);
-      $repo_baseurl=$1 if ($line =~ /(.*)(stretch|stable)/);
+      $repo_name=$1 if ($line =~ /\/([^\/]*)\n$/);
+      $repo_baseurl=$1 if ($line =~ /^(\S*)\h/);
 
       if ($line =~ /(^.*$)/ && $repo_name && $repo_baseurl){
           $common->addRepo({
