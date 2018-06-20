fcf67f57085c7ab03a8dfe58604719e08d21b645
Charlene Auger <charlene.auger@factorfx.com>
2018-06-20 11:26:49 +0200

    Update sql number

--- var.php
+++ var.php
@@ -71,6 +71,10 @@ define('PASSWORD_COMPAT', __DIR__ . '/libraries/password_compat/password.php');
  * Library PHPMAILER
  */
 define('PHPMAILER', __DIR__ . '/libraries/PHPMailer-6.0.5/src');
+/**
+ * Template Mail Directory
+ */
+define('TEMPLATE', __DIR__.'/require/mail/Templates/');
 //====================================================================================
 // GUI Options
 //====================================================================================
