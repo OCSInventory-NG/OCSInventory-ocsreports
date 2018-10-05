9e73fb74bfefe98e15659a467f7e50ec20b284cc
Cyrille bollu <cyrille.bollu2@gmail.com>
2018-10-05 10:05:41 +0200

    Implemented configurable cve-search integration

--- require/function_table_html.php
+++ require/function_table_html.php
@@ -536,20 +536,25 @@ function ajaxtab_entete_fixe($columns, $default_fields, $option = array(), $list
                 return vendor;
             };
             // get CVE info from a CVE-Search instance for a given software
-            function getCVE(row) {
+                // Get important data
                 var nameColumn = row.find(".NAME,.NAME sorting_1");
                 var name = nameColumn.text().toLowerCase();
+                var vendor = row.find(".PUBLISHER,.PUBLISHER sorting_1").text().toLowerCase();
                 // Don't execute this function for updates and security updates
                 if (name.startsWith("update for") | name.startsWith("security update for") | name.startsWith("service pack")){return};
+                // normalize data to better comply with CPE dictionary format
                 name = CpeNormalizeName(name);
-                var vendor = row.find(".PUBLISHER,.PUBLISHER sorting_1").text().toLowerCase();
                 if (vendor == '') {vendor=name};
                 vendor = CpeNormalizeVendor(vendor);
-                var cveUrl = "http://127.0.0.1:5000/api/search/" + vendor + "/" + name;
+                // Get URL of the cve-search instance to query from the config
+                var cveUrl = "<?php $h=look_config_default_values(array('VULN_CVESEARCH_HOST'));echo $h['tvalue']['VULN_CVESEARCH_HOST'] ?>";
+                console.log(cveUrl);
+                cveUrl = cveUrl + "/api/search/" + vendor + "/" + name;
                 // Add bug icon
                 var newContent = nameColumn.text();
                 newContent = newContent + '&nbsp<img src="image/bug-grey.png">';
                 nameColumn.html(newContent);
+                // Query the cve-search instance via AJAX
                 cveAjaxRequests[cveAjaxRequests.length] = $.ajax({
                     url: cveUrl,
                     dataType: 'json',
@@ -562,6 +567,10 @@ function ajaxtab_entete_fixe($columns, $default_fields, $option = array(), $list
                             newContent = newContent + '&nbsp<img src="image/bug-green.png">';
                         }
                         this.html(newContent);
+                    },
+                    error: function() {
+                        var newContent = this.text();
+                        newContent = newContent + '&nbsp<img src="image/bug-crossed.png">';
                     }
                 });
             };
@@ -593,17 +602,21 @@ function ajaxtab_entete_fixe($columns, $default_fields, $option = array(), $list
                     }
                     // For tables listing softwares, try to get CVE info
                     if (table_id == 'table#affich_soft') {
-                        // Abort any pending ajax requests
-                        if (cveAjaxRequests.length>0) {
-                            for (i=0;i<cveAjaxRequests.length;i++){
-                                cveAjaxRequests[i].abort();
+                        // Only if cve-search integration is enabled
+                        var cveEnabled = "<?php $h=look_config_default_values(array('VULN_CVESEARCH_ENABLE'));echo $h['ivalue']['VULN_CVESEARCH_ENABLE'] ?>";
+                        if (cveEnabled==1) {
+                            // Abort any pending ajax requests
+                            if (cveAjaxRequests.length>0) {
+                                for (i=0;i<cveAjaxRequests.length;i++){
+                                    cveAjaxRequests[i].abort();
+                                }
+                                cveAjaxRequests = [];
                             }
-                            cveAjaxRequests = [];
+                            // For data rows only
+                            $(table_id).find(".odd,.even").each(function(){
+                                getCVE($(this));
+                            });
                         }
-                        // For data rows only
-                        $(table_id).find(".odd,.even").each(function(){
-                            getCVE($(this));
-                        });
                     }
                 });
         <?php
