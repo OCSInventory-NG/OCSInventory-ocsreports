af6b3fc804df7bb760d76a78744edd69181419c8
Charlene Auger <charlene.auger@factorfx.com>
2019-01-02 14:53:34 +0100

    Fix bug array const php 5.5

--- require/extensions/ExtensionManager.php
+++ require/extensions/ExtensionManager.php
@@ -22,52 +22,52 @@
  */
 
 class ExtensionManager{
-    
+
     /**
      * Attributes
      */
     public $installedExtensionsList = null;
     public $installableExtensionsList = null;
     public $errorMessage = null;
-    
+
     /**
      * Constants
      */
-    
+
     // Extensions directories
     const MAIN_SEC_EXT = 'main_sections';
     const COMP_DETAIL_EXT = 'computer_detail';
     const LANGUAGE_EXT = 'language';
     const APACHE_EXT = 'apache';
     const AGENT_EXT = 'agent';
-    
+
     // Json infos of the extension
     const INFOS_EXT = 'infos.json';
-    
+
     // Extensions fobiden name
-    const FORBIDEN_EXT_NAME = [
+    private $FORBIDEN_EXT_NAME = [
         ".",
         "..",
         "readme.md"
     ];
-    
+
     // Extensions install required method
     const EXTENSION_INSTALL_METHD = 'extension_install_';
     const EXTENSION_DELETE_METHD = 'extension_delete_';
     const EXTENSION_UPGRADE_METHD = 'extension_upgrade_';
     const EXTENSION_HOOK_METHD = 'extension_hook_';
-    
+
     /**
      * List query
      */
     private $selectQuery = "SELECT * FROM `extensions`";
     private $selectQueryTarget = "SELECT * FROM `extensions` WHERE id = '%s'";
-    
+
     /**
      * Insert query
      */
     private $insertQuery = "INSERT INTO `extensions`(`id`, `name`, `description`, `version`, `licence`, `author`, `contributor`) VALUES ('%s','%s','%s',%s,'%s','%s','%s')";
-    
+
     /**
      * Delete query
      */
@@ -77,7 +77,7 @@ class ExtensionManager{
      * Objects
      */
     private $dbObject = null;
-    
+
     /**
      * Constructor
      */
@@ -86,35 +86,35 @@ class ExtensionManager{
         $this->dbObject = $_SESSION['OCS']["readServer"];
         $this->getInstalledExtensionsList();
     }
-    
+
     /**
-     * Will set an array of valid extensions 
+     * Will set an array of valid extensions
      */
     public function checkInstallableExtensions(){
-        
+
         // Scan dir and get all sub directory in extensions directory
         $items = scandir(EXT_DL_DIR);
         $installableExtList = [];
         foreach ($items as $index => $name) {
-            if(in_array($name, self::FORBIDEN_EXT_NAME) || !is_dir(EXT_DL_DIR.$name)){
+            if(in_array($name, $this->FORBIDEN_EXT_NAME) || !is_dir(EXT_DL_DIR.$name)){
                 continue;
             }
-            
+
             if($this->isExtensionCompliant($name)){
                 $installableExtList[] = $name;
-            }        
+            }
         }
-        
+
         $this->installableExtensionsList = $installableExtList;
     }
-    
+
     /**
      * Check if extensions is compliant to OCS Inventory Model
      */
     private function isExtensionCompliant($name){
         try{
             require EXT_DL_DIR.$name."/install.php";
-            
+
             if(
                 function_exists(self::EXTENSION_INSTALL_METHD.$name) &&
                 function_exists(self::EXTENSION_DELETE_METHD.$name) &&
@@ -123,34 +123,34 @@ class ExtensionManager{
             ){
                 return true;
             }
-            
+
             return true;
         } catch (Exception $ex) {
             return false;
         }
     }
-    
+
     /**
      * Will check pre-requisites for extensions installation
      */
     public function checkPrerequisites(){
         return true;
     }
-    
+
     /**
      * Extension installation
      */
     public function installExtension($name){
-        
+
         if($this->isInstalled($name)){
             // TODO : error already installed
             return false;
         }
-        
+
         // Add plugin record in database
         $jsonStr = file_get_contents(EXT_DL_DIR.$name."/".self::INFOS_EXT);
         $jsonInfos = json_decode($jsonStr, true);
-        
+
         $queryArrayArgs = [];
         $queryArrayArgs[] = $name;
         $queryArrayArgs[] = $jsonInfos['displayName'];
@@ -159,9 +159,9 @@ class ExtensionManager{
         $queryArrayArgs[] = $jsonInfos['licence'];
         $queryArrayArgs[] = $jsonInfos['author'][0];
         $queryArrayArgs[] = $jsonInfos['author'][0];
-        
+
         mysql2_query_secure($this->insertQuery, $this->dbObject, $queryArrayArgs);
-        
+
         try{
             $installMethod = self::EXTENSION_INSTALL_METHD.$name;
             $installMethod();
@@ -181,11 +181,11 @@ class ExtensionManager{
      * @return bool : true is ok else false
      */
     public function deleteExtension($name){
-        
+
         if(!$this->isInstalled($name)){
             return false;
         }
-        
+
         try{
             mysql2_query_secure($this->deleteQuery, $this->dbObject, $name);
             $deleteMethod = self::EXTENSION_DELETE_METHD.$name;
@@ -194,37 +194,37 @@ class ExtensionManager{
         } catch (Exception $ex) {
             return false;
         }
-        
+
     }
-    
+
     /**
      * Check if already installed
      */
     private function isInstalled($name){
-        
+
         if(in_array($name, $this->installedExtensionsList)){
             return true;
         }else{
             return false;
         }
-        
+
     }
 
     /**
      * Will set an array of installed extensions
      */
     private function getInstalledExtensionsList(){
-        
+
         $selectResult = mysql2_query_secure($this->selectQuery, $this->dbObject);
         $installedExt = [];
-        
+
         while($installedExtRows = $selectResult->fetch_array())
         {
             $installedExt[] = $installedExtRows['id'];
         }
-        
+
         $this->installedExtensionsList = $installedExt;
-        
+
     }
-    
+
 }
