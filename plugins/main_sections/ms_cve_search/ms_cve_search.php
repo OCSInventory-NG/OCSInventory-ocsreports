51d0bbcfb4964f1b7a667b437318f26c34545daf
charleneauger <charlene.auger50@gmail.com>
2019-06-12 10:23:59 +0200

    Not display table if not enabled

--- plugins/main_sections/ms_cve_search/ms_cve_search.php
+++ plugins/main_sections/ms_cve_search/ms_cve_search.php
@@ -34,79 +34,80 @@ $cve = new Cve();
 
 printEnTete($l->g(1463));
 
-//form name
-$form_name = 'cve_form';
-//form open
-echo open_form($form_name, '', '', 'form-horizontal');
-
-//definition of onglet
-$def_onglets['ALL_CVE'] = $l->g(1465); //All CVE.
-$def_onglets['0-1'] = $l->g(1468).' [ 0-1 ]';
-$def_onglets['1-2'] = $l->g(1468).' [ 1-2 ]';
-$def_onglets['2-3'] = $l->g(1468).' [ 2-3 ]';
-$def_onglets['3-4'] = $l->g(1468).' [ 3-4 ]';
-$def_onglets['4-5'] = $l->g(1468).' [ 4-5 ]';
-$def_onglets['5-6'] = $l->g(1468).' [ 5-6 ]';
-$def_onglets['6-7'] = $l->g(1468).' [ 6-7 ]';
-$def_onglets['7-8'] = $l->g(1468).' [ 7-8 ]';
-$def_onglets['8-9'] = $l->g(1468).' [ 8-9 ]';
-$def_onglets['9-10'] = $l->g(1468).' [ 9-10 ]';
-$def_onglets['10'] = $l->g(1468).' [ 10 ]';
-
-//default => first onglet
-if ($protectedPost['onglet'] == "") {
-    $protectedPost['onglet'] = "ALL_CVE";
-}
-
-//show first ligne of onglet
-show_tabs($def_onglets,$form_name,"onglet",true);
-
-echo '<div class="col col-md-10">';
-
 if($cve->CVE_ACTIVE != 1){
-    msg_warning($l->g(1464));
-}
-msg_warning($l->g(1466));
+    $msg = $l->g(1464)."</br>".$l->g(1466);
+    msg_info($msg);
+}else{
+    //form name
+    $form_name = 'cve_form';
+    //form open
+    echo open_form($form_name, '', '', 'form-horizontal');
 
-/******************************* ALL CVE *******************************/
-if($protectedPost['onglet'] == "ALL_CVE"){
-    $sql['SQL'] = 'SELECT *, link as id FROM cve_search GROUP BY link, cvss, soft, cve';
-}
+    //definition of onglet
+    $def_onglets['ALL_CVE'] = $l->g(1465); //All CVE.
+    $def_onglets['0-1'] = $l->g(1468).' [ 0-1 ]';
+    $def_onglets['1-2'] = $l->g(1468).' [ 1-2 ]';
+    $def_onglets['2-3'] = $l->g(1468).' [ 2-3 ]';
+    $def_onglets['3-4'] = $l->g(1468).' [ 3-4 ]';
+    $def_onglets['4-5'] = $l->g(1468).' [ 4-5 ]';
+    $def_onglets['5-6'] = $l->g(1468).' [ 5-6 ]';
+    $def_onglets['6-7'] = $l->g(1468).' [ 6-7 ]';
+    $def_onglets['7-8'] = $l->g(1468).' [ 7-8 ]';
+    $def_onglets['8-9'] = $l->g(1468).' [ 8-9 ]';
+    $def_onglets['9-10'] = $l->g(1468).' [ 9-10 ]';
+    $def_onglets['10'] = $l->g(1468).' [ 10 ]';
 
-/******************************* PER VULNERABILITIES *******************************/
-else{
-    if($protectedPost['onglet'] != '10'){
-        $vulnerability = explode('-', $protectedPost['onglet']);
-        $sql['SQL'] = 'SELECT *, link as id FROM cve_search WHERE cvss >= '. doubleval($vulnerability[0]) .' AND cvss < '. doubleval($vulnerability[1]) .' GROUP BY link, cvss, soft, cve';  
-    }else{
-        $sql['SQL'] = 'SELECT *, link as id FROM cve_search WHERE cvss = '. addslashes($protectedPost['onglet']) .' GROUP BY link, cvss, soft, cve';
+    //default => first onglet
+    if ($protectedPost['onglet'] == "") {
+        $protectedPost['onglet'] = "ALL_CVE";
     }
-}
 
-if (isset($sql)) {
-    $list_fields = array($l->g(69) => 'vendor',
-        'soft' => 'soft',
-        'Version' => 'version',
-        'CVSS' => 'cvss',
-        'CVE' => 'cve',
-        'link' => 'link'
-    );
-    $default_fields = $list_fields;
-    $list_col_cant_del = $default_fields;
-    $tab_options['LIEN_LBL']['soft'] = 'index.php?' . PAG_INDEX . '=' . $pages_refs['ms_multi_search'] . '&prov=allsoft&value=';
-    $tab_options['LIEN_CHAMP']['soft'] = 'soft';
-    $tab_options['LBL']['soft'] = $l->g(847);
-    $tab_options['LIEN_LBL']['link'] = ' ';
-    $tab_options['LIEN_CHAMP']['link'] = 'link';
-    $tab_options['LBL']['link'] = $l->g(1467);
-    $tab_options['ARG_SQL'] = $sql['ARG'];
-    $tab_options['form_name'] = $form_name;
-    $tab_options['table_name'] = $form_name;
-    $result_exist = ajaxtab_entete_fixe($list_fields, $default_fields, $tab_options, $list_col_cant_del);
-}
+    //show first ligne of onglet
+    show_tabs($def_onglets,$form_name,"onglet",true);
 
-echo "</div>";
-echo close_form();
+    echo '<div class="col col-md-10">';
+
+    /******************************* ALL CVE *******************************/
+    if($protectedPost['onglet'] == "ALL_CVE"){
+        $sql['SQL'] = 'SELECT *, link as id FROM cve_search GROUP BY link, cvss, soft, cve';
+    }
+
+    /******************************* PER VULNERABILITIES *******************************/
+    else{
+        if($protectedPost['onglet'] != '10'){
+            $vulnerability = explode('-', $protectedPost['onglet']);
+            $sql['SQL'] = 'SELECT *, link as id FROM cve_search WHERE cvss >= '. doubleval($vulnerability[0]) .' AND cvss < '. doubleval($vulnerability[1]) .' GROUP BY link, cvss, soft, cve';  
+        }else{
+            $sql['SQL'] = 'SELECT *, link as id FROM cve_search WHERE cvss = '. addslashes($protectedPost['onglet']) .' GROUP BY link, cvss, soft, cve';
+        }
+    }
+
+    if (isset($sql)) {
+        $list_fields = array($l->g(69) => 'vendor',
+            'soft' => 'soft',
+            'Version' => 'version',
+            'CVSS' => 'cvss',
+            'CVE' => 'cve',
+            'link' => 'link'
+        );
+        $default_fields = $list_fields;
+        $list_col_cant_del = $default_fields;
+        $tab_options['LIEN_LBL']['soft'] = 'index.php?' . PAG_INDEX . '=' . $pages_refs['ms_multi_search'] . '&prov=allsoft&value=';
+        $tab_options['LIEN_CHAMP']['soft'] = 'soft';
+        $tab_options['LBL']['soft'] = $l->g(847);
+        $tab_options['LIEN_LBL']['link'] = ' ';
+        $tab_options['LIEN_CHAMP']['link'] = 'link';
+        $tab_options['LBL']['link'] = $l->g(1467);
+        $tab_options['ARG_SQL'] = $sql['ARG'];
+        $tab_options['form_name'] = $form_name;
+        $tab_options['table_name'] = $form_name;
+        $result_exist = ajaxtab_entete_fixe($list_fields, $default_fields, $tab_options, $list_col_cant_del);
+    }
+
+    echo "</div>";
+    echo close_form();
+
+}
 
 if (AJAX) {
     ob_end_clean();
