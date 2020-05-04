b43f0365d17ea6eeaf582d173494377d096faa32
Charlene Auger <root@OCS-srv-trunk2-ocs.factorfx.com>
2020-05-04 09:42:22 +0000

    Fix software count on dashboard

--- require/console/Console.php
+++ require/console/Console.php
@@ -140,7 +140,7 @@
         $result_os = mysql2_query_secure($sql_os, $_SESSION['OCS']["readServer"]);
         $oss = "<p style='font-size:32px; font-weight:bold;'>".mysqli_num_rows($result_os)."</p>";
         //get softwares
-        $sql = "SELECT ID, count(ID) FROM `software` group by ID";
+        $sql = "SELECT ID, count(CONCAT(NAME_ID,'_',VERSION_ID)) FROM `software` GROUP BY CONCAT(NAME_ID,'_', VERSION_ID)";
         $result = mysql2_query_secure($sql, $_SESSION['OCS']["readServer"]);
         $softs = "<a style='font-size:32px; font-weight:bold;' href='index.php?function=visu_all_soft'>".mysqli_num_rows($result)."</a>";
 
