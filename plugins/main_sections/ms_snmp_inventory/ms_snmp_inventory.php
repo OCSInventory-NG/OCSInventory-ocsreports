a487b8d94ac8b47fe50250756bed95160d0ba97a
Charlene Auger <root@OCS-srv-trunk2-ocs.factorfx.com>
2020-05-12 14:35:12 +0000

    Add SNMP Inventory page

---
+++ plugins/main_sections/ms_snmp_inventory/ms_snmp_inventory.php
@@ -0,0 +1,67 @@
+<?php
+/*
+ * Copyright 2005-2016 OCSInventory-NG/OCSInventory-ocsreports contributors.
+ * See the Contributors file for more details about them.
+ *
+ * This file is part of OCSInventory-NG/OCSInventory-ocsreports.
+ *
+ * OCSInventory-NG/OCSInventory-ocsreports is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 2 of the License,
+ * or (at your option) any later version.
+ *
+ * OCSInventory-NG/OCSInventory-ocsreports is distributed in the hope that it
+ * will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OCSInventory-NG/OCSInventory-ocsreports. if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+if (AJAX) {
+    parse_str($protectedPost['ocs']['0'], $params);
+    $protectedPost += $params;
+    ob_start();
+}
+
+require_once('require/snmp/Snmp.php');
+require_once('require/function_machine.php');
+
+$snmp = new OCSSnmp();
+
+$typeList = $snmp->get_all_type();
+
+foreach($typeList as $key => $value) {
+
+    $list_fields = [];
+    $tab_options = [];
+
+    print_item_header($typeList[$key]['TYPENAME']);
+
+    $form_name = $typeList[$key]['TABLENAME'];
+    $table_name = $form_name;
+
+    echo open_form($form_name, '', '', 'form-horizontal');
+
+    $tab_options = $protectedPost;
+    $tab_options['form_name'] = $form_name;
+    $tab_options['table_name'] = $table_name;
+    $list_fields = $snmp->show_columns($typeList[$key]['TABLENAME']);
+    $list_col_cant_del = $list_fields;
+    $default_fields = $list_fields;
+    $tab_options['FILTRE'] = array_flip($list_fields);
+    $queryDetails = "SELECT * FROM ".$typeList[$key]['TABLENAME'];
+
+    ajaxtab_entete_fixe($list_fields, $default_fields, $tab_options, $list_col_cant_del);
+    echo close_form();
+
+    if (AJAX) {
+        ob_end_clean();
+        tab_req($list_fields, $default_fields, $list_col_cant_del, $queryDetails, $tab_options);
+        ob_start();
+    }
+}
+
+
