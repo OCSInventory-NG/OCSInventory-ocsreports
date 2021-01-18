20c35b8b524c9b9c7f1b6bef54a25f3b61372d5d
Charlene Auger <charlene.auger@factorfx.com>
2021-01-18 10:02:17 +0000

    Replace dynamic xml value and add frag

--- require/teledeploy/PackageBuilder.php
+++ require/teledeploy/PackageBuilder.php
@@ -29,7 +29,15 @@ class PackageBuilder
 	private $packageBuilderForm;
 	private $packageBuilderParseXml;
 	private $downloadConfig = [];
-
+	
+	/**
+	 * Method __construct
+	 *
+	 * @param $packageBuilderForm $packageBuilderForm [explicite description]
+	 * @param $packageBuilderParseXml $packageBuilderParseXml [explicite description]
+	 *
+	 * @return void
+	 */
 	function __construct($packageBuilderForm, $packageBuilderParseXml) {
 		$this->packageBuilderForm = $packageBuilderForm;
 		$this->packageBuilderParseXml = $packageBuilderParseXml;
@@ -40,7 +48,15 @@ class PackageBuilder
 			'DOWNLOAD_RATIO_FRAG' => 'DOWNLOAD_RATIO_FRAG',
 		]);
 	}
-
+	
+	/**
+	 * Method buildPackage
+	 *
+	 * @param $post $post [explicite description]
+	 * @param $file $file [explicite description]
+	 *
+	 * @return void
+	 */
 	public function buildPackage($post, $file) {
 		global $l;
 		
@@ -54,14 +70,21 @@ class PackageBuilder
 		}
 
 		if(file_exists($file["additionalfiles"]["tmp_name"])) {
+			// Create folder if not exists
 			if (!file_exists($downloadPath)) {
                 mkdir($downloadPath);
 			}
+			// Create package archive
 			$details = $this->fragmentPackage($file["additionalfiles"]["tmp_name"], $downloadPath, $timestamp);
 		}
 
+		// Get Xml option info
 		$xmlDetails = $this->packageBuilderParseXml->parseOptions($post['FORMTYPE']);
+		// Replace dynamic value from xml
+		$xmlDetails = $this->replaceXmlValue($post, $xmlDetails);
+		// Generate info xml
 		$info = $this->writePackageInfo($xmlDetails, $timestamp, $details['frag'], $digest, $post['pathfile']);
+		// Create info file
 		$handinfo = fopen($downloadPath."/info", "w+");
         fwrite($handinfo, $info);
 		fclose($handinfo);
@@ -90,43 +113,92 @@ class PackageBuilder
 		msg_success($l->g(437) . " " . $downloadPath);
 		//delete cache for activation
 		unset($_SESSION['OCS']['DATA_CACHE']['LIST_PACK']);
-		unset($_SESSION['OCS']['NUM_ROW']['LIST_PACK']);		
+		unset($_SESSION['OCS']['NUM_ROW']['LIST_PACK']);
 
-	}
-
-	private function fragmentPackage($fname, $downloadPath, $timestamp) {
-		// If package fragmentation disabled, frag = 1
-		if($this->downloadConfig['ivalue']['DOWNLOAD_ACTIVATE_FRAG'] != 1) {
-			$size = @filesize($fname);
-			$handle = fopen($fname, "rb");
-
-			$read = 0;
-			$frag = 1;
-
-            for ($i = 1; $i < $frag; $i++) {
-                $contents = fread($handle, $size / $frag);
-                $read += strlen($contents);
-                $handfrag = fopen($downloadPath."/".$timestamp."-".$i, "w+b");
-                fwrite($handfrag, $contents);
-                fclose($handfrag);
-            }
-
-            $contents = fread($handle, $size - $read);
-            $read += strlen($contents);
-			$handfrag = fopen($downloadPath."/".$timestamp."-".$i, "w+b");
-			
-            fwrite($handfrag, $contents);
-            fclose($handfrag);
-			fclose($handle);
-			
-			$details['frag'] = $i;
-			$details['size'] = $size;
-
-			return $details;
-		}
 	}
 	
-	private function writePackageInfo($xmlDetails, $timestamp, $nbFrag, $digest, $path) {
+	/**
+	 * Method replaceXmlValue
+	 *
+	 * @param $post $post [explicite description]
+	 * @param $xmlDetails $xmlDetails [explicite description]
+	 *
+	 * @return void
+	 */
+	private function replaceXmlValue($post, $xmlDetails) {
+		foreach($xmlDetails->packagedefinition as $packagedefinition) {
+			foreach ($packagedefinition as $key => $value) {
+				if(strpos($value, ":") !== false) {
+					$xmlDetails->packagedefinition->$key = $post[ltrim($value, ':')];	
+				}
+			}	
+		}
+
+		return $xmlDetails;
+	}
+	
+	/**
+	 * Method fragmentPackage
+	 *
+	 * @param $fname $fname [explicite description]
+	 * @param $downloadPath $downloadPath [explicite description]
+	 * @param $timestamp $timestamp [explicite description]
+	 *
+	 * @return void
+	 */
+	private function fragmentPackage($fname, $downloadPath, $timestamp) {
+		$size = @filesize($fname);
+
+		// If package fragmentation disabled, frag = 1
+		if($this->downloadConfig['ivalue']['DOWNLOAD_ACTIVATE_FRAG'] == 1 && $this->downloadConfig['ivalue']['DOWNLOAD_RATIO_FRAG'] != null) {
+			$frag = 0;
+			$sizeBis = $size / pow(1024, 2);
+
+			while($sizeBis > 0) {
+				$sizeBis = $sizeBis - intval($this->downloadConfig['ivalue']['DOWNLOAD_RATIO_FRAG']);
+				$frag ++;
+			}
+		} else {
+			$frag = 1;
+		}
+		
+		$handle = fopen($fname, "rb");
+		$read = 0;
+		
+		for ($i = 1; $i < $frag; $i++) {
+			$contents = fread($handle, $size / $frag);
+			$read += strlen($contents);
+			$handfrag = fopen($downloadPath."/".$timestamp."-".$i, "w+b");
+			fwrite($handfrag, $contents);
+			fclose($handfrag);
+		}
+
+		$contents = fread($handle, $size - $read);
+		$read += strlen($contents);
+		$handfrag = fopen($downloadPath."/".$timestamp."-".$i, "w+b");
+		
+		fwrite($handfrag, $contents);
+		fclose($handfrag);
+		fclose($handle);
+		
+		$details['frag'] = $i;
+		$details['size'] = $size;
+
+		return $details;
+	}
+	
+	/**
+	 * Create Info package file
+	 * 
+	 * @param obj $xmlDetails
+	 * @param int $timestamp
+	 * @param int $nbFrag
+	 * @param int $digest
+	 * 
+	 * @return string $info
+	 */
+	private function writePackageInfo($xmlDetails, $timestamp, $nbFrag, $digest) {
+
 		$info  = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
         $info .= "<DOWNLOAD ID=\"" . $timestamp . "\" " .
                  "PRI=\"" . $xmlDetails->packagedefinition->PRI . "\" " .
@@ -137,13 +209,13 @@ class PackageBuilder
                  "DIGEST_ALGO=\"MD5\" " .
                  "DIGEST_ENCODE=\"Hexa\" ";
         if ($xmlDetails->packagedefinition->ACT  == 'STORE') {
-            $info .= "PATH=\"" . $path . "\" ";
+            $info .= "PATH=\"" . $xmlDetails->packagedefinition->PATH . "\" ";
         }
         if ($xmlDetails->packagedefinition->ACT  == 'LAUNCH') {
-            $info .= "NAME=\"" . $path . "\" ";
+            $info .= "NAME=\"" . $xmlDetails->packagedefinition->NAME . "\" ";
         }
         if ($xmlDetails->packagedefinition->ACT  == 'EXECUTE') {
-            $info .= "COMMAND=\"" . $path . "\" ";
+            $info .= "COMMAND=\"" . $xmlDetails->packagedefinition->COMMAND . "\" ";
         }
 
         $info .= "NOTIFY_USER=\"" . $xmlDetails->packagedefinition->NOTIFY_USER  . "\" " .
@@ -158,9 +230,14 @@ class PackageBuilder
 		return $info;
 		
 	}
-
+	
 	/**
-	 * Get Package FileID
+	 * Method getPackageFileId
+	 *
+	 * @param $name $name [explicite description]
+	 * @param $session $session [explicite description]
+	 *
+	 * @return void
 	 */
 	public function getPackageFileId($name , $session = null) {
 		
