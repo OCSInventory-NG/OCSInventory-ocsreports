6890cf4dc52f2973142bd0adcd75692fb43e253f
Charlene Auger <charlene.auger@factorfx.com>
2021-01-21 14:14:47 +0000

    Add scheduled task script for update agent on windows

---
+++ config/teledeploy/script/scheduledupdateagent.ps1
@@ -0,0 +1,10 @@
+# Move ocsupdate.exe on Temp file
+Move-Item .\*.exe -Destination C:\Temp\ocsupdate.exe
+
+# Unregister old ocs inventory scheduled task if exists
+Unregister-ScheduledTask -TaskName "OCS Inventory Agent Update"  -Confirm:$false
+
+# Register new ocs inventory scheduled task
+$action = New-ScheduledTaskAction -Execute 'C:\Temp\ocsupdate.exe'
+$trigger =  New-ScheduledTaskTrigger -Once -At ((Get-Date).AddMinutes(30))
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "OCS Inventory Agent Update" -Description "Scheduled task to update OCS Inventory Agent" -RunLevel Highest
