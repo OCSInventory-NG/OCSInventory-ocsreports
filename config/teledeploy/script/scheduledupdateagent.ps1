9d77c7003cfc0230f456b986ee112ad3d1c3add7
Charlene Auger <charlene.auger@factorfx.com>
2021-01-21 16:14:06 +0000

    Add exit 1 if error

--- config/teledeploy/script/scheduledupdateagent.ps1
+++ config/teledeploy/script/scheduledupdateagent.ps1
@@ -2,9 +2,13 @@
 Move-Item .\*.exe -Destination C:\Temp\ocsupdate.exe
 
 # Unregister old ocs inventory scheduled task if exists
-Unregister-ScheduledTask -TaskName "OCS Inventory Agent Update"  -Confirm:$false
+Unregister-ScheduledTask -TaskName "OCS Inventory Agent Update" -Confirm:$false
 
 # Register new ocs inventory scheduled task
 $action = New-ScheduledTaskAction -Execute 'C:\Temp\ocsupdate.exe'
-$trigger =  New-ScheduledTaskTrigger -Once -At ((Get-Date).AddMinutes(30))
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "OCS Inventory Agent Update" -Description "Scheduled task to update OCS Inventory Agent" -RunLevel Highest
+$trigger = New-ScheduledTaskTrigger -Once -At ((Get-Date).AddMinutes(30))
+$result = Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "OCS Inventory Agent Update" -Description "Scheduled task to update OCS Inventory Agent" -RunLevel Highest
+
+if(!$result) {
+	exit 1
+}
