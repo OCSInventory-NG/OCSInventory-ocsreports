a792bfddff97932c94a9e59f370e2fcd49898c77
Lea9250 <lea.droguet@factorfx.com>
2021-10-25 13:09:18 +0000

    change software id from int to big int

---
+++ files/update/7053.sql
@@ -0,0 +1,13 @@
+-- save current setting of sql_mode
+SET @old_sql_mode := @@sql_mode ;
+
+-- derive a new value by removing NO_ZERO_DATE and NO_ZERO_IN_DATE
+SET @new_sql_mode := @old_sql_mode ;
+SET @new_sql_mode := TRIM(BOTH ',' FROM REPLACE(CONCAT(',',@new_sql_mode,','),',NO_ZERO_DATE,'  ,','));
+SET @new_sql_mode := TRIM(BOTH ',' FROM REPLACE(CONCAT(',',@new_sql_mode,','),',NO_ZERO_IN_DATE,',','));
+SET @@sql_mode := @new_sql_mode ;
+
+ALTER TABLE software MODIFY COLUMN ID BIGINT(20) NOT NULL AUTO_INCREMENT;
+
+-- revert back to the original sql_mode setting
+SET @@sql_mode := @old_sql_mode ;
