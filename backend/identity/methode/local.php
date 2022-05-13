96b32ab2cb9047ece352a1daa52711a4865e37dc
Lea9250 <lea.droguet@factorfx.com>
2022-05-13 09:38:56 +0000

    display correct error msg when user with no rights

--- backend/identity/methode/local.php
+++ backend/identity/methode/local.php
@@ -54,7 +54,7 @@ if (isset($rowOp->accesslvl)) {
     $profile = $profile_serializer->unserialize($lvluser, file_get_contents($profile_config));
 
     $restriction = $profile->getRestriction('GUI');
-
+    $restrictions = $profile->getRestrictions();
     //Si l'utilisateur a des droits limités
     //on va rechercher les tags sur lesquels il a des droits
     if ($restriction == 'YES') {
@@ -79,9 +79,16 @@ if (isset($rowOp->accesslvl)) {
                 $list_tag[$row->tag] = $row->tag;
             }
         }
+
         if (!isset($list_tag)) {
             $ERROR = $l->g(893);
         }
+
+        // if user is restricted on all pages and has no tag assigned, he has the right to connect but no access
+        if (!isset($list_tag) && !in_array('NO', $restrictions)) {
+            $ERROR = $l->g(896);
+        }
+
     } elseif ($restriction != 'NO') {
         $ERROR = $restriction;
     }
