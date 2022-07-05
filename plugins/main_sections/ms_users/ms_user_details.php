89f44880c8b40f17b9fe9338b2b656223251cd3e
Lea9250 <lea.droguet@factorfx.com>
2022-07-05 14:31:07 +0000

    use protectedPost on user details

--- plugins/main_sections/ms_users/ms_user_details.php
+++ plugins/main_sections/ms_users/ms_user_details.php
@@ -33,7 +33,7 @@ if (!AJAX) {
 require_once 'require/function_users.php';
 
 if (isset($protectedPost['Valid_modif'])) {
-    $msg = add_user($_POST, get_profile_labels());
+    $msg = add_user($protectedPost, get_profile_labels());
     if ($msg != $l->g(374)) {
         msg_error($msg);
     } else {
