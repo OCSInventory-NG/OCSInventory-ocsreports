fbebb4d844c54ced7e98255c456e1f30039581ce
Lea9250 <lea.droguet@factorfx.com>
2022-09-14 17:20:55 +0200

    working mail notifications

--- crontab/cron_report_notification.php
+++ crontab/cron_report_notification.php
@@ -7,13 +7,24 @@ require_once('../require/config/include.php');
 require_once('../require/fichierConf.class.php');
 require_once('../require/function_table_html.php');
 require_once('../require/groupReports/GroupReport.php');
+require_once('../require/mail/NotificationMail.php');
 
 $_SESSION['OCS']["writeServer"] = dbconnect(SERVER_WRITE, COMPTE_BASE, PSWD_BASE, DB_NAME, SSL_KEY, SSL_CERT, CA_CERT, SERVER_PORT);
 $_SESSION['OCS']["readServer"] = dbconnect(SERVER_READ, COMPTE_BASE, PSWD_BASE, DB_NAME, SSL_KEY, SSL_CERT, CA_CERT, SERVER_PORT);
 
+// getting scheduled group notifications
+$groupReport = new GroupReport();
+$scheduled = $groupReport->getScheduledReports();
 
-$report = new GroupReport();
-$scheduled = $report->getScheduledReports();
+// retrieve smtp config
+$notif = new NotificationMail(DEFAULT_LANGUAGE);
+$values = $notif->get_info_smtp();
+
+// send mail
+if ($scheduled != '') {
+    $sendNotifs = $groupReport->sendReportNotification($scheduled, $values, $groupReport);
+}
+
 
 
 
--- require/groupReports/GroupReport.php
+++ require/groupReports/GroupReport.php
@@ -21,6 +21,9 @@
  * MA 02110-1301, USA.
  */
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
 /**
  * Class for generating and sending dynamic group reports
@@ -35,7 +38,6 @@ class GroupReport {
         $recurrences = array("DAILY"  =>  "((LAST_EXEC <= NOW() - INTERVAL 1 DAY AND LAST_EXEC >= NOW() - INTERVAL 1 WEEK) OR (LAST_EXEC = DATE_CREATED AND RECURRENCE = 'DAILY'))",
                              "WEEKLY"  =>  " WEEKDAY = WEEKDAY(NOW()) AND ((LAST_EXEC <= NOW() - INTERVAL 1 WEEK AND LAST_EXEC >= NOW() - INTERVAL 1 MONTH) OR (LAST_EXEC = DATE_CREATED AND RECURRENCE = 'WEEKLY'))",
                              "MONTHLY"  =>  "((LAST_EXEC <= NOW() - INTERVAL 1 WEEK AND LAST_EXEC <= NOW() - INTERVAL 1 MONTH) OR (LAST_EXEC = DATE_CREATED AND RECURRENCE = 'MONTHLY'))");
-
 
         foreach ($recurrences as $key => $recurrence) {
             $sqlRec = "SELECT * FROM `reports_notifications` WHERE STATUS = 'ON' AND (END_DATE = 0 OR END_DATE >= NOW()) AND $recurrence";
@@ -56,15 +58,16 @@ class GroupReport {
             // get group ids associated with reports
             $strIds = implode(",", $ids);
             $scheduledCount = count($ids);
-            error_log("[".date("Y-m-d H:i:s")."] Found $scheduledCount reports scheduled to run. Now getting related group data .. \n");
+            error_log("[".date("Y-m-d H:i:s")."] Found $scheduledCount reports scheduled to run. Now getting related group data ..");
             $groupData = $this->getGroupData($strIds);
-            error_log("[".date("Y-m-d H:i:s")."] Now generating $scheduledCount reports \n");
+            error_log("[".date("Y-m-d H:i:s")."] Now generating $scheduledCount reports");
             $reports = $this->generateReport($reportsData, $groupData);
             error_log("[".date("Y-m-d H:i:s")."] Reports generated to ".VARLIB_DIR."/tmp_dir/, sending notifications .. ");
-            $notif = $this->mailReports($reports);
         } else {
-            error_log("[".date("Y-m-d H:i:s")."] No reports scheduled to be sent today, exiting script. \n");
+            error_log("[".date("Y-m-d H:i:s")."] No reports scheduled to be sent today, exiting script.");
         }
+
+        return $reports ?? '';
 	}
 
     /**
@@ -104,24 +107,29 @@ class GroupReport {
      */
     public function generateReport($reportsData, $groupData) {
         $reports = array();
+        
         foreach ($reportsData as $report) {
             // report title is built from dynamic grp name
             $report['TITLE'] = $groupData[$report['GROUP_ID']]['NAME'];
-            $fileName = $report['TITLE']."_".date("Y-m-d_H:i:s").".xlsx";
+            $now = date("Y-m-d_H:i:s");
+            $fileName = $report['TITLE']."_".$now.".xlsx";
+            $filePath = VARLIB_DIR."/tmp_dir/$fileName";
             $heading = false;
             
 
             if(is_writable(dirname($fileName))) {
                 $fp = fopen(VARLIB_DIR."/tmp_dir/$fileName", 'w');
                 foreach ($groupData as $data) {
-
+                    $ids = array();
+                    $lines = array();
                     $query = $this->regeneration_sql($data['XMLDEF']);
                     $reportResult = mysql2_query_secure($query[1], $_SESSION['OCS']["readServer"]);
     
                     while ($value = mysqli_fetch_array($reportResult)) {
                         $ids[] = $value["ID"];
                     }
-    
+                    
+                    
                     $strIds = implode(",", $ids);
                     // from list of IDs, get all hardware info for these devices
                     $deviceQuery = "SELECT SQL_CALC_FOUND_ROWS h.ID,h.DEVICEID,h.name,h.OSNAME,h.OSVERSION,h.OSCOMMENTS,h.PROCESSORT,h.PROCESSORS,h.PROCESSORN,h.MEMORY,h.SWAP,h.LASTDATE,h.LASTCOME,h.QUALITY,h.FIDELITY,h.DESCRIPTION,h.IPADDR,h.userid,b.ssn,h.ID 
@@ -131,15 +139,19 @@ class GroupReport {
                     while( $row = mysqli_fetch_assoc($reportResult)) {
                         $lines[] = $row;
                     }
-    
+                    
+
+                    // fetching the export sep from OCS configuration
+                    $sep = look_config_default_values("EXPORT_SEP");
+                    $sep = !empty($sep['tvalue']['EXPORT_SEP']) ? $sep['tvalue']['EXPORT_SEP'] : ';';
                     // writing the file
                     if(!empty($lines)) {
                         foreach($lines as $line) {
                             if(!$heading) {
-                                fwrite($fp, implode("\t", array_keys($line)) . "\n");
+                                fwrite($fp, implode($sep, array_keys($line)) . "\n");
                                 $heading = true;
                             } else {
-                                fwrite($fp, implode("\t", array_values($line)) . "\n");
+                                fwrite($fp, implode($sep, array_values($line)) . "\n");
                             }
                         }
                     }
@@ -147,7 +159,8 @@ class GroupReport {
                 
                 fclose($fp);
                 $report['FILE'] = $fileName;
-
+                $report['FILEPATH'] = $filePath;
+                $report['DATE'] = $now;
                 $reports[$report['GROUP_ID']] = $report;
 
             } else {
@@ -157,13 +170,100 @@ class GroupReport {
         }
 
         return $reports;
+
+    }
+
+    /**
+     * Update LAST_EXEC field + remove files after they were sent
+     */
+    public function updateLastExec($id) {
+    
+        if (isset($id)) {
+            $updateQuery = "UPDATE `reports_notifications` SET LAST_EXEC = NOW() WHERE ID = $id";
+            $reportResult = mysql2_query_secure($updateQuery, $_SESSION['OCS']["writeServer"]);
+        }
+    }
+
+
+
+    public function mailTemplate($report, $template) {
+        $template = file_get_contents($template);
+        $placeholders = array(
+                        '/{TITLE}/',
+                        '/{DATE}/',
+                        '/{GROUP_ID}/',
+                    );
+            $replacements = array(
+                                $report['TITLE'],
+                                $report['DATE'],
+                                $report['GROUP_ID'],
+                            );
+        $template = preg_replace($placeholders, $replacements, $template);
         
-
+        return $template;
     }
 
-    public function mailReports($reports) {
+    public function sendReportNotification($reports, $values, $groupReport) {
+    // writing and sending notifications if notifications have been set
+    if($values['NOTIF_FOLLOW'] == 'ON'){
+            foreach ($reports as $id => $report) {
+                $mail = new PHPMailer();
+                //$mail->SMTPDebug  = 3; 
 
+                // Set mailer to use SMTP
+                $mail->isSMTP();
+                $mail->Host = $values['NOTIF_SMTP_HOST'];
+                $mail->SMTPSecure = $values['NOTIF_SEND_MODE'];
+                $mail->Port = $values['NOTIF_PORT_SMTP'];
+
+                if ($values['NOTIF_USER_SMTP'] != '' && $values['NOTIF_PASSWD_SMTP'] != '') {
+                    // Enable SMTP authentication
+                    $mail->SMTPAuth = true;
+                    // SMTP username
+                    $mail->Username = $values['NOTIF_USER_SMTP'];
+                    // SMTP password
+                    $mail->Password = $values['NOTIF_PASSWD_SMTP'];
+                } else {
+                    $mail->SMTPAuth = false;
+                }
+
+                if($values['NOTIF_MAIL_REPLY'] != '' && $values['NOTIF_NAME_REPLY'] != ''){
+                    $mail->addReplyTo($values['NOTIF_MAIL_REPLY'], $values['NOTIF_NAME_REPLY']);
+                }
+
+                // set From
+                $mail->setFrom($values['NOTIF_MAIL_ADMIN'], $values['NOTIF_NAME_ADMIN']);
+
+                // set recipients
+                $recipients = explode(',', json_decode($report['MAIL']));
+                $mail->addAddress($recipients[0]);
+                unset($recipients[0]);
+
+                foreach ($recipients as $recipient) {
+                    $mail->addCC($recipient);
+                }
+
+                // set report in attachment
+                $mail->addAttachment($report['FILEPATH']);
+                $mail->Subject = "OCS Inventory dynamic group report : ".$report['TITLE'];
+                // html format
+                $mail->isHTML(true); 
+                $mail->Body = $groupReport->mailTemplate($report, TEMPLATE.'OCS_group_notif.html');
+                $send = $mail->send();
+                
+                if ($send) {
+                    echo "[".date("Y-m-d H:i:s")."] Message has been sent successfully \n";
+                    // updating last_exec datetime + removes generated files from tmp_dir
+                    $groupReport->updateLastExec($report['ID']);
+                    echo "[".date("Y-m-d H:i:s")."] Files removed from tmp_dir/ \n";
+                } else {
+                    echo "[".date("Y-m-d H:i:s")."] Mailer Error: " . $mail->ErrorInfo . "\n";
+                }
+                unlink($report['FILEPATH']);
+            }
+        }
     }
+
 }
 
 
