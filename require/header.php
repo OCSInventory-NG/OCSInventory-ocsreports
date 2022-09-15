32d25c876dea3e1f5fab7eacc385817ba0c746f4
Charlene Auger <charlene.auger@factorfx.com>
2022-09-15 11:28:04 +0200

    Call class Admininfo in header

--- require/header.php
+++ require/header.php
@@ -360,8 +360,10 @@ if (!isset($_SESSION['OCS']["usecache"]) || !isset($_SESSION['OCS']["tabcache"])
 
 /* * *******************************************GESTION OF LBL_TAG************************************ */
 if (!isset($_SESSION['OCS']['TAG_LBL'])) {
-    require_once('require/function_admininfo.php');
-    $all_tag_lbl = witch_field_more('COMPUTERS');
+    require_once('require/admininfo/Admininfo.php');
+    $Admininfo = new Admininfo();
+
+    $all_tag_lbl = $Admininfo->witch_field_more('COMPUTERS');
     foreach ($all_tag_lbl['LIST_NAME'] as $key => $value) {
         $_SESSION['OCS']['TAG_LBL'][$value] = $all_tag_lbl['LIST_FIELDS'][$key];
         $_SESSION['OCS']['TAG_ID'][$key] = $value;
