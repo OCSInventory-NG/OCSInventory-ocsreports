0e2555d9aaded897fe14671153d28ea7682ab9eb
Charlene Auger <charlene.auger@factorfx.com>
2022-10-13 09:06:05 +0000

    fix multisearch duplicate fields

--- plugins/main_sections/ms_multi_search/ms_multi_search.php
+++ plugins/main_sections/ms_multi_search/ms_multi_search.php
@@ -216,6 +216,17 @@ if($protectedPost['onglet'] == "COMPUTERS") {
 	
 		foreach ($_SESSION['OCS']['multi_search'] as $table => $infos) {
 			$i = 0;
+
+			//Check if all values have fields
+			foreach ($infos as $key => $values) {
+				if (!array_key_exists('fields', $values)) {
+					unset($infos[$key]);
+					if (array_key_exists($key, $_SESSION['OCS']['multi_search'][$table])) {
+						unset($_SESSION['OCS']['multi_search'][$table][$key]);
+					}
+				}
+			}
+			
 			foreach ($infos as $uniqid => $values) {
 				?>
 				<div class="row" name="<?php echo $uniqid ?>">
