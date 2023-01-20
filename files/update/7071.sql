92a01b2728f4c7b58d03585c875acaea8b741f14
Antoine ROBIN <antoine.robin@factorfx.com>
2023-01-20 14:42:04 +0000

    fix: update file

--- files/update/7071.sql
+++ files/update/7071.sql
@@ -1,2 +1,2 @@
 -- add new bios interface feature
-INSERT INTO `config` VALUES('INTERFACE_LAST_CONTACT',1,'','Enable BIOS stat diagram');
+INSERT INTO `config` VALUES('INTERFACE_BIOS_DIAGRAM_VISIBILITY',1,'','Enable BIOS stat diagram');
