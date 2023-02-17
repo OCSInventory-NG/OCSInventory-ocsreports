a94e6e9c76c1ed435c841c19a030a7c5a6c4cb9e
Charlene Auger <charlene.auger@factorfx.com>
2023-02-17 14:37:57 +0100

    Fix require file path in all crontab

--- crontab/cron_all_software.php
+++ crontab/cron_all_software.php
@@ -1,11 +1,11 @@
 #!/usr/bin/php
 <?php
-require_once('../var.php');
+require_once(__DIR__.'/../var.php');
 require_once(CONF_MYSQL);
-require_once('../require/function_commun.php');
-require_once('../require/softwares/AllSoftware.php');
-require_once('../require/config/include.php');
-require_once('../require/fichierConf.class.php');
+require_once(ETC_DIR.'/require/function_commun.php');
+require_once(ETC_DIR.'/require/softwares/AllSoftware.php');
+require_once(ETC_DIR.'/require/config/include.php');
+require_once(ETC_DIR.'/require/fichierConf.class.php');
 
 $_SESSION['OCS']["writeServer"] = dbconnect(SERVER_WRITE, COMPTE_BASE, PSWD_BASE, DB_NAME, SSL_KEY, SSL_CERT, CA_CERT, SERVER_PORT);
 $_SESSION['OCS']["readServer"] = dbconnect(SERVER_READ, COMPTE_BASE, PSWD_BASE, DB_NAME, SSL_KEY, SSL_CERT, CA_CERT, SERVER_PORT);
