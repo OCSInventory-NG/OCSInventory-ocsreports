bc71142e4403c73faaad54b1b243b16fb124bfd6
Charlene Auger <charlene.auger@factorfx.com>
2023-04-13 17:01:19 +0200

    Add chunk and help arguments to script execution

--- crontab/cron_all_software.php
+++ crontab/cron_all_software.php
@@ -10,8 +10,29 @@ require_once(ETC_DIR.'/require/fichierConf.class.php');
 $_SESSION['OCS']["writeServer"] = dbconnect(SERVER_WRITE, COMPTE_BASE, PSWD_BASE, DB_NAME, SSL_KEY, SSL_CERT, CA_CERT, SERVER_PORT);
 $_SESSION['OCS']["readServer"] = dbconnect(SERVER_READ, COMPTE_BASE, PSWD_BASE, DB_NAME, SSL_KEY, SSL_CERT, CA_CERT, SERVER_PORT);
 
+$shortOptions = "c:h::";
+$longOptions = array("chunk:","help::");
+
+$options = getopt($shortOptions, $longOptions);
+
+$chunk = 1000;
+
+if(array_key_exists("h", $options) || array_key_exists("help", $options)) {
+    echo "Usage: php cron_all_software.php [--] [args...]\n\n";
+    echo "  -c, --chunk <number>    Process software by pool of <number>, default 5000\n\n";
+    exit();
+}
+
+if(array_key_exists("c", $options) || array_key_exists("chunk", $options)) {
+    $chunk = $options["c"] ?? $options["chunk"];
+}
+
 $software = new AllSoftware();
 
-error_log(print_r("Please wait, software processing is in progress. It could take a few minutes ...", true));
+print("[".date("Y-m-d H:i:s"). "] Start to clean orphan software\n");
 $cleanup = $software->software_cleanup();
-$insert = $software->software_link_treatment();
+print("[".date("Y-m-d H:i:s"). "] End of clean\n");
+
+print("[".date("Y-m-d H:i:s"). "] Start to process software by pool of ".$chunk."\n");
+$insert = $software->software_link_treatment($chunk);
+print("[".date("Y-m-d H:i:s"). "] End of process\n");
