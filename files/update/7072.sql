cf5442ce3907b2664dd024bf8ce36d4ea11914d9
Lea9250 <lea.droguet@factorfx.com>
2023-05-15 11:40:40 +0200

    feat(config): add generation of snmp files to general configuration

--- files/update/7072.sql
+++ files/update/7072.sql
@@ -2,3 +2,4 @@
 INSERT INTO `config` VALUES('SCAN_TYPE_IPDISCOVER',0,'ICMP','IPD scan type');
 INSERT INTO `config` VALUES('SCAN_TYPE_SNMP',0,'ICMP','SNMP scan type');
 INSERT INTO `config` VALUES('SCAN_ARP_BANDWIDTH',256,'','Arp scan bandwidth in Kb/s');
+INSERT INTO `config` VALUES('GENERATE_OCS_FILES_SNMP',0,'','Generate SNMP files');
