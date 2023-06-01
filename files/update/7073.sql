8257e08f9ed2560ac6ff61f5a5079ed5181b50ba
Charlene Auger <charlene.auger@factorfx.com>
2023-06-01 11:53:13 +0000

    Add major, minor and patch column for software_version

--- files/update/7073.sql
+++ files/update/7073.sql
@@ -1,5 +1,11 @@
 -- Add pretty version column for software_version
-ALTER TABLE `software_version` ADD COLUMN `PRETTY_VERSION` VARCHAR(255) DEFAULT NULL;
+ALTER TABLE `software_version` ADD COLUMN `PRETTYVERSION` VARCHAR(255) DEFAULT NULL;
+-- Add major version column for software_version
+ALTER TABLE `software_version` ADD COLUMN `MAJOR` INT DEFAULT NULL;
+-- Add minor version column for software_version
+ALTER TABLE `software_version` ADD COLUMN `MINOR` INT DEFAULT NULL;
+-- Add patch version column for software_version
+ALTER TABLE `software_version` ADD COLUMN `PATCH` INT DEFAULT NULL;
 
 -- Remove VULN_CVESEARCH_ALL from config
 DELETE FROM `config` WHERE `NAME` = 'VULN_CVESEARCH_ALL';
