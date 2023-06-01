d6944029d57035d5f87f4d29a39a6715e8ca62e1
Charlene Auger <charlene.auger@factorfx.com>
2023-06-01 11:55:19 +0000

    Add new software columns

--- require/search/SoftwareSearch.php
+++ require/search/SoftwareSearch.php
@@ -34,6 +34,7 @@
     public $tables = [
         self::SOFTWARE_TABLE,
         self::NAME_TABLE,
+        self::VERSION_TABLE
     ];
 
     private $dbObject;
@@ -54,7 +55,11 @@
         foreach ($this->tables as $table) {
             $columnsList = mysql2_query_secure("SHOW COLUMNS FROM %s", $this->dbObject, $table);
             while ($columnsInfos = mysqli_fetch_array($columnsList)) {
-                if($table == self::SOFTWARE_TABLE || $columnsInfos['Field'] == "CATEGORY") {
+                if($table == self::SOFTWARE_TABLE 
+                || $columnsInfos['Field'] == "PRETTYVERSION"
+                || $columnsInfos['Field'] == "MAJOR"
+                || $columnsInfos['Field'] == "MINOR"
+                || $columnsInfos['Field'] == "PATCH") {
                     $columnsInfos[DatabaseSearch::TYPE] = $this->normalizeFieldType($columnsInfos['Type']);
                     $this->columnsList[self::SOFTWARE_TABLE][$columnsInfos['Field']] = [
                         DatabaseSearch::FIELD => $columnsInfos[DatabaseSearch::FIELD],
@@ -96,8 +101,11 @@
             break;
             case "VERSION_ID" :
                 return self::VERSION_TABLE;
-            case "CATEGORY" :
-                return self::NAME_TABLE;
+            case "PRETTYVERSION" :
+            case "MAJOR" :
+            case "MINOR" :
+            case "PATCH" :
+                return self::VERSION_TABLE;
             break;
             default :
                 return self::SOFTWARE_TABLE;
