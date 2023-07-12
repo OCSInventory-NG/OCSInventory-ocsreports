d5fd351d8c377584c1d49020df714d3a9fd8230a
Charlene Auger <charlene.auger@factorfx.com>
2023-07-12 09:11:56 +0000

    Update typo

--- crontab/cron_clean_orphan.php
+++ crontab/cron_clean_orphan.php
@@ -9,7 +9,7 @@ require_once(ETC_DIR.'/require/fichierConf.class.php');
 $_SESSION['OCS']["writeServer"] = dbconnect(SERVER_WRITE, COMPTE_BASE, PSWD_BASE, DB_NAME, SSL_KEY, SSL_CERT, CA_CERT, SERVER_PORT);
 $_SESSION['OCS']["readServer"] = dbconnect(SERVER_READ, COMPTE_BASE, PSWD_BASE, DB_NAME, SSL_KEY, SSL_CERT, CA_CERT, SERVER_PORT);
 
-$query = "SELECT CONCAT('DELETE FROM ',TABLE_NAME,\" WHERE HARDWARE_ID  NOT IN (SELECT ID FROM hardware);\") as deletequery, TABLE_NAME FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '%s' AND COLUMN_NAME='HARDWARE_ID';";
+$query = "SELECT CONCAT('DELETE FROM `',TABLE_NAME,'` WHERE HARDWARE_ID  NOT IN (SELECT ID FROM hardware);') as deletequery, TABLE_NAME FROM information_schema.COLUMNS WHERE TABLE_SCHEMA = '%s' AND COLUMN_NAME='HARDWARE_ID';";
 $args = array(DB_NAME);
 
 $queryList = mysql2_query_secure($query, $_SESSION['OCS']["readServer"], $args);
@@ -19,13 +19,13 @@ print("[".date("Y-m-d H:i:s"). "] Start to clean orphan HARDWARE_ID\n");
 if($queryList) {
     foreach($queryList as $key => $values) {
         print("[".date("Y-m-d H:i:s"). "] Clean orphan HARDWARE_ID from ".$values["TABLE_NAME"]."\n");
-        $countDeletetionRowQuery = "SELECT DISTINCT HARDWARE_ID FROM %s WHERE HARDWARE_ID  NOT IN (SELECT ID FROM hardware)";
+        $countDeletetionRowQuery = "SELECT DISTINCT HARDWARE_ID FROM `%s` WHERE HARDWARE_ID  NOT IN (SELECT ID FROM hardware)";
         $args = array($values["TABLE_NAME"]);
         $countDeletetionRow = mysql2_query_secure($countDeletetionRowQuery, $_SESSION['OCS']["readServer"], $args);
-        
+
         $deleteResult = mysql2_query_secure($values["deletequery"], $_SESSION['OCS']["writeServer"]);
 
-        if($deleteResult) print("[".date("Y-m-d H:i:s"). "] ".$countDeletetionRow->num_rows." orphan HARDWARE_ID has been deleted from ".$values["TABLE_NAME"]."\n");
+        if($deleteResult) print("[".date("Y-m-d H:i:s"). "] ".$countDeletetionRow->num_rows." orphan HARDWARE_ID have been deleted from ".$values["TABLE_NAME"]."\n");
         else print("[".date("Y-m-d H:i:s"). "] An error occurs when attempt to clean orphan HARDWARE_ID from ".$values["TABLE_NAME"]."\n");
     }
 }
